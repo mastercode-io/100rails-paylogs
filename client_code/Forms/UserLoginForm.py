be38371ad0ae559aada9b70686aa5da742779e42
Alex Sherin <al@mastercode.io>
2023-09-26 14:52:51 +0100

    Alex remote

--- client_code/Forms/UserLoginForm.py
+++ client_code/Forms/UserLoginForm.py
@@ -20,14 +20,19 @@ class UserLoginForm(FormBase):
         validation = {
             'rules': {
                 self.login.el_id: {'required': True},
-                # self.password.el_id: {'required': True},
+                self.password.el_id: {'required': True},
             }
         }
 
         super().__init__(fields=fields, validation=validation, button_save_label='Login', **kwargs)
+        self.form.showCloseIcon = True
+        self.form.buttons = [
+            {'buttonModel': {'isPrimary': True, 'content': 'LOGIN'}, 'click': self.login_user},
+            {'buttonModel': {'content': 'Forgot Password'}, 'click': self.forgot_password},
+        ],
 
 
-    def form_save(self, args):
+    def login_user(self, args):
         print('Logging user...', args)
         try:
             user = anvil.users.login_with_email(self.login.value, self.password.value, remember=self.remember.value)
@@ -39,3 +44,11 @@ class UserLoginForm(FormBase):
             self.error.message = f'Invalid login details: {e}'
             self.error.type = 'e-error'
             return
+
+
+    def forgot_password(self, args):
+        print('Forgot password', args)
+        anvil.users.send_password_reset_email(self.login.value)
+        self.error.message = f'Password reset email sent to {self.login.value}'
+        self.error.type = 'e-info'
+        return
