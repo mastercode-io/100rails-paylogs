c3fc17b58ea9a0a61599c0af079b3c42e8b2358f
Alex Sherin <al@mastercode.io>
2023-10-08 16:08:01 +0100

    Alex remote

--- client_code/Forms/TimesheetTypeForm.py
+++ client_code/Forms/TimesheetTypeForm.py
@@ -11,14 +11,13 @@ class TimesheetTypeForm(FormBase):
         self.name = TextInput(name='name', label='Name')
         self.short_code = TextInput(name='short_code', label='Short Code')
         self.description = MultiLineInput(name='description', label='Description')
-        self.notes = MultiLineInput(name='notes', label='Notes')
         self.configuration= MultiFieldInput(name='configuration', model='TimesheetType',cols=2)
 
         sections = [
             {
               'name': '_', 'rows': [
                 {self.name, self.short_code},
-                {self.description, self.notes},
+                {self.description},
                 {self.configuration}
             ]
             },
