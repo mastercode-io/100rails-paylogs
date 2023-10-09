94965071ae2b3992ef2a260f578c290ccb8f5ef6
Alex Sherin <al@mastercode.io>
2023-10-09 16:37:06 +0100

    Alex remote

---
+++ client_code/Pages/ImportRecordsPage.py
@@ -0,0 +1,53 @@
+from AnvilFusion.components.PageBase import PageBase
+from AnvilFusion.components.FormInputs import *
+from anvil.js.window import ej
+import uuid
+
+
+MODELS_LIST = [
+    'Employee',
+    'Job',
+    'Timesheet'
+]
+
+
+class ImportRecordsPage(PageBase):
+    def __init__(self, **kwargs):
+        print('ImportRecordsPage')
+        title = 'Import Records'
+        self.select_model = DropdownInput(name='select_model', label='Select Model', options=MODELS_LIST)
+        self.import_button = ej.buttons.Button({
+            'content': 'Migrate DB',
+            'isPrimary': True,
+            'size': 'large',
+        })
+        self.import_button_id = f'migrate-button-{uuid.uuid4()}'
+        self.execution_log = InlineMessage(name='execution_log')
+
+        self.content = f'<br><div id="{self.select_model.container_id}"></div><br><br>'
+        self.content += f'<br><div id="{self.import_button_id}"></div><br><br>'
+        self.content += f'<div id="{self.execution_log.container_id}" style="overflow-y: scroll; height: 100%;"></div>'
+
+        super().__init__(page_title=title, content=self.content, overflow='auto', **kwargs)
+
+
+    def form_show(self, **args):
+        # print('MigratePage.form_show')
+        super().form_show(**args)
+        # anvil.js.window.document.getElementById(self.container_id).style.overflow = 'hidden'
+        self.import_button.appendTo(f'#{self.import_button_id}')
+        # self.migrate_button.addEventListener('onclick', self.import_button_action)
+        self.import_button.element.onclick = self.import_button_action
+        self.execution_log.show()
+        self.execution_log.message = 'Click <b>Migrate DB</b> to start migration'
+
+
+    def import_button_action(self, args):
+        print('import_button_action')
+        self.execution_log.message = f'Importing {self.select_model.value} records<br><br>'
+
+        self.execution_log.message += '<br>Import complete.'
+
+
+    def log_message(self, message):
+        self.execution_log.message += str(message) + '<br>'
--- client_code/app/HomePage/navigation.py
+++ client_code/app/HomePage/navigation.py
@@ -52,6 +52,7 @@ PL_SIDEBAR_MENUS = {
         {'nodeId': 'settings_pay_categories', 'nodeText': 'Pay Categories', 'nodeChild': []},
         {'nodeId': 'settings_pay_rate_rules', 'nodeText': 'Pay Rate Rules', 'nodeChild': []},
         {'nodeId': 'settings_pay_rate_templates', 'nodeText': 'Pay Rate Templates', 'nodeChild': []},
+        {'nodeId': 'settings_import_records', 'nodeText': 'Import Records', 'nodeChild': []},
     ],
     'admin_menu': [
         {'nodeId': 'admin_tenants', 'nodeText': 'Tenants', 'nodeChild': []},
@@ -101,6 +102,7 @@ PL_NAV_ITEMS = {
     'settings_pay_categories': {'model': 'PayCategory', 'type': 'view', 'action': 'open', 'props': {}},
     'settings_pay_rate_rules': {'model': 'PayRateRule', 'type': 'view', 'action': 'open', 'props': {}},
     'settings_pay_rate_templates': {'model': 'PayRateTemplate', 'type': 'view', 'action': 'open', 'props': {}},
+    'settings_import_records': {'type': 'page', 'name': 'ImportRecordsPage', 'action': 'open', 'props': {}},
 
     'admin_tenants': {'model': 'Tenant', 'type': 'view', 'action': 'open', 'props': {}},
     'admin_users': {'model': 'User', 'type': 'view', 'action': 'open', 'props': {}},
