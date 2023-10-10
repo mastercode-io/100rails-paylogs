6854edfd8c29bd7164ba1215acf93c993650f648
Alex Sherin <al@mastercode.io>
2023-10-10 21:07:32 +0100

    Alex remote

--- client_code/Forms/__init__.py
+++ client_code/Forms/__init__.py
@@ -1,14 +1,14 @@
 from .CreateTenantForm import CreateTenantForm
 
 from .UserLoginForm import UserLoginForm
-from .UserForm import UserForm
+# from .UserForm import UserForm
 from .BusinessForm import BusinessForm
 from .EmployeeForm import EmployeeForm
 from .EmployeeRoleForm import EmployeeRoleForm
 from .LocationForm import LocationForm
 from .TimesheetTypeForm import TimesheetTypeForm
-from .TimesheetForm import TimesheetForm
-from .PayRunForm import PayRunForm
-from .PayCategoryForm import PayCategoryForm
-from .PayRateTemplateForm import PayRateTemplateForm
-from .PayRateRuleForm import PayRateRuleForm
+# from .TimesheetForm import TimesheetForm
+# from .PayRunForm import PayRunForm
+# from .PayCategoryForm import PayCategoryForm
+# from .PayRateTemplateForm import PayRateTemplateForm
+# from .PayRateRuleForm import PayRateRuleForm
