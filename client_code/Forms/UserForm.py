7043085b477e29ab52445824001ef0cddd3f9b4d
Alex Sherin <al@mastercode.io>
2023-10-12 21:19:42 +0100

    Alex remote

--- client_code/Forms/UserForm.py
+++ client_code/Forms/UserForm.py
@@ -2,11 +2,17 @@ from AnvilFusion.components.FormBase import FormBase
 from AnvilFusion.components.FormInputs import *
 from AnvilFusion.components.MultiFieldInput import MultiFieldInput
 
+FORM_ACTION_HEADER = {
+    'add': 'Signup',
+    'edit': 'Update',
+}
+
 
 class UserForm(FormBase):
     def __init__(self, **kwargs):
         print('UserForm')
         kwargs['model'] = 'User'
+        action = kwargs.get('action', 'add')
 
         self.first_name = TextInput(name='first_name', label='First Name', required=True)
         self.last_name = TextInput(name='last_name', label='Last Name', required=True)
@@ -14,6 +20,11 @@ class UserForm(FormBase):
         self.enabled = CheckboxInput(name='enabled', label='Enabled')
         self.user_roles = LookupInput(name='user_roles', label='Roles', model='UserRole', multiple=True)
         self.permissions = MultiFieldInput(name='permissions', model='User', label='Permissions')
+
+        buttons = [
+            {'buttonModel': {'isPrimary': True, 'content': FORM_ACTION_HEADER[action]}, 'click': self.save_user},
+            {'buttonModel': {'isPrimary': True, 'content': 'Cancel'}, 'click': self.form_cancel},
+        ]
 
         fields = [
             self.first_name,
@@ -24,5 +35,17 @@ class UserForm(FormBase):
             self.permissions,
         ]
 
-        super().__init__(fields=fields, **kwargs)
-        # self.fullscreen = True
+        super().__init__(
+            header=f"{FORM_ACTION_HEADER[action]} User",
+            fields=fields,
+            buttons=buttons,
+            **kwargs
+        )
+
+
+    def save_user(self, args):
+        pass
+
+
+    def form_cancel(self, args):
+        super().form_cancel(args)
