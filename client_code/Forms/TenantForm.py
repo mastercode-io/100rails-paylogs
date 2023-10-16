f0b53b6466a2012b7ccaa07db07c8311180d760e
Alex Sherin <al@mastercode.io>
2023-10-16 12:19:47 +0100

    Alex remote

--- client_code/Forms/TenantForm.py
+++ client_code/Forms/TenantForm.py
@@ -78,7 +78,7 @@ class TenantForm(FormBase):
     def form_open(self, args):
         super().form_open(args)
         print('TenantForm.form_open')
-        print(dict(self.data), self.data.uid)
+        print(dir(self.data), self.data.uid)
         if self.data.uid:
             AppEnv.set_tenant(tenant_uid=self.data.uid)
             business = Business.search()
