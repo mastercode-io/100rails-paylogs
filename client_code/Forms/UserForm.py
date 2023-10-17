ccf2af95b2874ed8ac2882aeb5bc69e28eee580a
Alex Sherin <al@mastercode.io>
2023-10-17 13:21:25 +0100

    Alex remote

--- client_code/Forms/UserForm.py
+++ client_code/Forms/UserForm.py
@@ -52,7 +52,10 @@ class UserForm(FormBase):
         super().form_open(args)
         print('UserForm.form_show', self.action)
         print('source', self.source)
-        print('source value', getattr(self.source, 'value'))
+        source_value = getattr(self.source, 'value', None)
+        print('source value', source_value)
+        if source_value is not None:
+            print('tenant_uid', getattr(source_value, 'tenant_uid'))
         if self.action == 'edit':
             self.password.hide()
             self.confirm_pwd.hide()
