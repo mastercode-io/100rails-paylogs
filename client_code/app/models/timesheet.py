52e50b5806d13e337a6933d3093d691e8ab436e0
Alex Sherin <al@mastercode.io>
2023-10-23 12:19:54 +0100

    Alex remote

--- client_code/app/models/timesheet.py
+++ client_code/app/models/timesheet.py
@@ -34,26 +34,30 @@ class TimesheetType:
 
     )
 
+@model_type
+class Timesheet:
+    _title = "employee"
 
-# @model_type
-# class Timesheet:
-#     _title = "employee"
-#
-#     timesheet_type = Relationship("TimesheetType")
-#     employee = Relationship("Employee")
-#     payrun = Relationship("PayRun")
-#     job = Relationship("Job")
-#     date = Attribute(field_type=types.FieldTypes.DATE)
-#     start_time = Attribute(field_type=types.FieldTypes.DATETIME)
-#     end_time = Attribute(field_type=types.FieldTypes.DATETIME)
-#     status = Attribute(field_type=types.FieldTypes.ENUM_SINGLE)
-#     approved_by = Relationship("Employee")
-#     notes = Attribute(field_type=types.FieldTypes.MULTI_LINE)
-#     total_pay = Attribute(field_type=types.FieldTypes.CURRENCY)
-#
-#     @staticmethod
-#     def calculate_total_hours(args):
-#         if args["start_time"] is None or args["end_time"] is None:
-#             return 0
-#         return (args["end_time"] - args["start_time"]).total_seconds() / 3600
-#     total_hours = Computed(("start_time", "end_time"), "calculate_total_hours")
+    timesheet_type = Relationship("TimesheetType")
+    employee = Relationship("Employee")
+    payrun = Relationship("PayRun")
+    job = Relationship("Job")
+    date = Attribute(field_type=types.FieldTypes.DATE)
+    start_time = Attribute(field_type=types.FieldTypes.DATETIME)
+    end_time = Attribute(field_type=types.FieldTypes.DATETIME)
+    status = Attribute(field_type=types.FieldTypes.ENUM_SINGLE)
+    approved_by = Relationship("Employee")
+    notes = Attribute(field_type=types.FieldTypes.MULTI_LINE)
+    total_pay = Attribute(field_type=types.FieldTypes.CURRENCY)
+
+    @staticmethod
+    def calculate_total_hours(args):
+        if args["start_time"] is None or args["end_time"] is None:
+            return 0
+        return (args["end_time"] - args["start_time"]).total_seconds() / 3600
+    total_hours = Computed(("start_time", "end_time"), "calculate_total_hours")
+
+
+# Module name correction for imports
+TimesheetType.__module__ = __name__[:__name__.rfind(".")]
+Timesheet.__module__ = __name__[:__name__.rfind(".")]
