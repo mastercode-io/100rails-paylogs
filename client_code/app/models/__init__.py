124f7854c25672f5d900a0d262e995da89696a6f
Alex Sherin <al@mastercode.io>
2023-10-23 11:30:31 +0100

    Alex remote

--- client_code/app/models/__init__.py
+++ client_code/app/models/__init__.py
@@ -7,6 +7,7 @@ from AnvilFusion.datamodel.particles import (
 from AnvilFusion.datamodel import types
 from datetime import datetime
 
+from timesheet import TimesheetType, Timesheet
 
 # Model list for enumerations
 ENUM_MODEL_LIST = {
@@ -344,53 +345,3 @@ class PayRun:
     def get_payrun_week(args):
         return  f"{args['pay_period_end'].year} - WK{args['pay_period_end'].isocalendar()[1]}"
     payrun_week = Computed(["pay_period_end"], "get_payrun_week")
-
-
-@model_type
-class Timesheet:
-    _title = "employee"
-
-    timesheet_type = Relationship("TimesheetType")
-    employee = Relationship("Employee")
-    payrun = Relationship("PayRun")
-    job = Relationship("Job")
-    date = Attribute(field_type=types.FieldTypes.DATE)
-    start_time = Attribute(field_type=types.FieldTypes.DATETIME)
-    end_time = Attribute(field_type=types.FieldTypes.DATETIME)
-    status = Attribute(field_type=types.FieldTypes.ENUM_SINGLE)
-    approved_by = Relationship("Employee")
-    notes = Attribute(field_type=types.FieldTypes.MULTI_LINE)
-    total_pay = Attribute(field_type=types.FieldTypes.CURRENCY)
-
-    @staticmethod
-    def calculate_total_hours(args):
-        if args["start_time"] is None or args["end_time"] is None:
-            return 0
-        return (args["end_time"] - args["start_time"]).total_seconds() / 3600
-    total_hours = Computed(("start_time", "end_time"), "calculate_total_hours")
-
-
-@model_type
-class TimesheetType:
-    _title = "short_code"
-
-    short_code = Attribute(field_type=types.FieldTypes.SINGLE_LINE)
-    name = Attribute(field_type=types.FieldTypes.SINGLE_LINE)
-    description = Attribute(field_type=types.FieldTypes.MULTI_LINE)
-    status = Attribute(field_type=types.FieldTypes.ENUM_SINGLE)
-
-    configuration_schema = {
-        "job_required": Attribute(field_type=types.FieldTypes.BOOLEAN),
-        "sick_leave": Attribute(field_type=types.FieldTypes.BOOLEAN),
-        "paid_time": Attribute(field_type=types.FieldTypes.BOOLEAN),
-        "annual_leave": Attribute(field_type=types.FieldTypes.BOOLEAN),
-        "paid_breaks": Attribute(field_type=types.FieldTypes.BOOLEAN),
-        "unpaid_leave": Attribute(field_type=types.FieldTypes.BOOLEAN),
-        "worked_time": Attribute(field_type=types.FieldTypes.BOOLEAN),
-        "other": Attribute(field_type=types.FieldTypes.BOOLEAN),
-        "break_time": Attribute(field_type=types.FieldTypes.BOOLEAN),
-        "break_length": Attribute(field_type=types.FieldTypes.NUMBER),
-    }
-    configuration = Attribute(
-        field_type=types.FieldTypes.OBJECT, schema=configuration_schema
-    )
---
+++ client_code/app/models/timesheet.py
@@ -0,0 +1,58 @@
+from AnvilFusion.datamodel.particles import (
+    model_type,
+    Attribute,
+    Relationship,
+    Computed,
+)
+from AnvilFusion.datamodel import types
+
+
+@model_type
+class TimesheetType:
+    _title = "short_code"
+
+    short_code = Attribute(field_type=types.FieldTypes.SINGLE_LINE)
+    name = Attribute(field_type=types.FieldTypes.SINGLE_LINE)
+    description = Attribute(field_type=types.FieldTypes.MULTI_LINE)
+    status = Attribute(field_type=types.FieldTypes.ENUM_SINGLE)
+
+    configuration_schema = {
+        "job_required": Attribute(field_type=types.FieldTypes.BOOLEAN),
+        "sick_leave": Attribute(field_type=types.FieldTypes.BOOLEAN),
+        "paid_time": Attribute(field_type=types.FieldTypes.BOOLEAN),
+        "annual_leave": Attribute(field_type=types.FieldTypes.BOOLEAN),
+        "paid_breaks": Attribute(field_type=types.FieldTypes.BOOLEAN),
+        "unpaid_leave": Attribute(field_type=types.FieldTypes.BOOLEAN),
+        "worked_time": Attribute(field_type=types.FieldTypes.BOOLEAN),
+        "other": Attribute(field_type=types.FieldTypes.BOOLEAN),
+        "break_time": Attribute(field_type=types.FieldTypes.BOOLEAN),
+        "break_length": Attribute(field_type=types.FieldTypes.NUMBER),
+    }
+    configuration = Attribute(
+        field_type=types.FieldTypes.OBJECT, schema=configuration_schema
+
+    )
+
+
+@model_type
+class Timesheet:
+    _title = "employee"
+
+    timesheet_type = Relationship("TimesheetType")
+    employee = Relationship("Employee")
+    payrun = Relationship("PayRun")
+    job = Relationship("Job")
+    date = Attribute(field_type=types.FieldTypes.DATE)
+    start_time = Attribute(field_type=types.FieldTypes.DATETIME)
+    end_time = Attribute(field_type=types.FieldTypes.DATETIME)
+    status = Attribute(field_type=types.FieldTypes.ENUM_SINGLE)
+    approved_by = Relationship("Employee")
+    notes = Attribute(field_type=types.FieldTypes.MULTI_LINE)
+    total_pay = Attribute(field_type=types.FieldTypes.CURRENCY)
+
+    @staticmethod
+    def calculate_total_hours(args):
+        if args["start_time"] is None or args["end_time"] is None:
+            return 0
+        return (args["end_time"] - args["start_time"]).total_seconds() / 3600
+    total_hours = Computed(("start_time", "end_time"), "calculate_total_hours")
