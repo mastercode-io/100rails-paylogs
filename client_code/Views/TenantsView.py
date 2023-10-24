23aa2ce3e1e7014ab56099df2409bda70c5269d7
Alex Sherin <al@mastercode.io>
2023-10-24 13:50:18 +0100

    Alex remote

---
+++ client_code/Views/TenantsView.py
@@ -0,0 +1,35 @@
+from AnvilFusion.components.GridView import GridView
+
+
+class TenantsView(GridView):
+    def __init__(self, **kwargs):
+        print('TenantsView')
+        view_config = {
+            'model': 'Tenant',
+            'columns': [
+                {'name': 'name', 'label': 'Account Name'},
+                {'name': 'status', 'label': 'Status'},
+                {'name': '_spacer'},
+            ],
+        }
+
+        context_menu_items = [
+            {'id': 'select_tenant', 'label': 'Select Tenant', 'action': self.select_tenant},
+            {'id': 'reset_tenant', 'label': 'Reset Tenant', 'action': self.reset_tenant},
+        ]
+
+        super().__init__(
+            model='Tenant',
+            view_config=view_config,
+            context_menu_items=context_menu_items,
+            **kwargs)
+
+
+    def select_tenant(self, args):
+        print('select_tenant', args)
+        pass
+
+
+    def reset_tenant(self, args):
+        print('reset_tenant', args)
+        pass
---
+++ client_code/Views/__init__.py
@@ -0,0 +1,1 @@
+from .TenantsView import TenantsView
