6017ca34598f9c2139191292cd33a6adf4b717f2
Alex Sherin <al@mastercode.io>
2023-11-21 20:14:57 +0000

    Alex remote

--- client_code/Views/TimesheetListView.py
+++ client_code/Views/TimesheetListView.py
@@ -7,17 +7,6 @@ class TimesheetListView(GridView):
     def __init__(self, **kwargs):
         print('TimesheetListView')
 
-        # timesheet_type = Relationship("TimesheetType")
-        # employee = Relationship("Employee")
-        # payrun = Relationship("Payrun")
-        # job = Relationship("Job")
-        # date = Attribute(field_type=types.FieldTypes.DATE)
-        # start_time = Attribute(field_type=types.FieldTypes.DATETIME)
-        # end_time = Attribute(field_type=types.FieldTypes.DATETIME)
-        # status = Attribute(field_type=types.FieldTypes.ENUM_SINGLE)
-        # approved_by = Relationship("Employee")
-        # notes = Attribute(field_type=types.FieldTypes.MULTI_LINE)
-        # total_pay = Attribute(field_type=types.FieldTypes.CURRENCY)
         view_config = {
             'model': 'Timesheet',
             'columns': [
@@ -33,10 +22,9 @@ class TimesheetListView(GridView):
             ],
         }
 
-        # context_menu_items = [
-        #     {'id': 'select_tenant', 'label': 'Lock Dataset', 'action': lock_dataset},
-        #     {'id': 'reset_tenant', 'label': 'Reset Dataset', 'action': reset_dataset},
-        # ]
+        context_menu_items = [
+            {'id': 'calculate_awards', 'label': 'Lock Dataset', 'action': self.calculate_awards},
+        ]
 
         super().__init__(
             model='Timesheet',
@@ -59,13 +47,6 @@ class TimesheetListView(GridView):
                 {'field': 'date', 'direction': 'Ascending'}
             ]
         }
-        # self.grid.editSettings = {
-        #     'allowEditing': True,
-        #     'allowAdding': False,
-        #     'allowDeleting': True,
-        #     'mode': 'Normal',
-        # }
-        # self.grid.dataBound = self.collapse_all
         self.first_load = True
 
 
@@ -76,3 +57,7 @@ class TimesheetListView(GridView):
         return (f'<div class="template" style="{caption_color}">'
                 f'{args.items[0].employee__full_name}</div>')
         # return args['due_date']
+
+
+    def calculate_awards(self, args):
+        print('calculate_awards', args)
