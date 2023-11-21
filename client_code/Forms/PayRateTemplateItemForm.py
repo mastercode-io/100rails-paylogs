9ed306677ec093a05c885483435d7207ca5d6d1f
Alex Sherin <al@mastercode.io>
2023-11-21 16:08:57 +0000

    Alex remote

--- client_code/Forms/PayRateTemplateItemForm.py
+++ client_code/Forms/PayRateTemplateItemForm.py
@@ -9,7 +9,8 @@ class PayRateTemplateItemForm(FormBase):
 
         self.order_number = NumberInput(name='order_number', label='Order', format='##')
         self.pay_rate_title = TextInput(name='pay_rate_title', label='Title')
-        self.pay_rate_rule = LookupInput(name='pay_rate_rule', label='Rule', model='PayRateRule')
+        self.pay_rate_rule = LookupInput(name='pay_rate_rule', label='Rule', model='PayRateRule',
+                                         on_change=self.pay_rate_rule_selected)
         self.pay_rate = NumberInput(name='pay_rate', label='Rate', format='c2')
         self.pay_rate_multiplier = NumberInput(name='pay_rate_multiplier', label='Multiplier', format='p2')
         self.status = RadioButtonInput(name='status', label='Status', options=['Active', 'Inactive'], value='Active')
@@ -24,3 +25,13 @@ class PayRateTemplateItemForm(FormBase):
         ]
 
         super().__init__(fields=fields, **kwargs)
+
+
+    def pay_rate_rule_selected(self, args):
+        if self.pay_rate_rule.value is None or args.get('value', None) is None:
+            self.pay_rate.value = None
+            self.pay_rate_multiplier.value = None
+        else:
+            self.pay_rate.value = self.pay_rate_rule.value.pay_rate
+            self.pay_rate_multiplier.value = self.pay_rate_rule.value.pay_rate_multiplier
+            self.pay_rate_title.value = self.pay_rate_rule.value['name']
