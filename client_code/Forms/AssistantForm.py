d005f155f8b06ac1aefdb490aa746f8ad5ae985e
Alex Sherin <al@mastercode.io>
2023-11-23 18:06:16 +0000

    Alex remote

--- client_code/Forms/AssistantForm.py
+++ client_code/Forms/AssistantForm.py
@@ -67,136 +67,137 @@ class AssistantForm:
         max_height = int(self.container_el.style['max-height'][0:-2])
         # self.chat_el.style.height = f'{max_height - 50}px'
         self.form.element.style.height = f'{max_height - 50}px'
-        self.chat = jQuery(f"#{self.chat_id}").kendoChat({
-            'user': {
-                'name': 'User',
-                'iconUrl': 'https://demos.telerik.com/kendo-ui/content/chat/agent-01.png'
-            },
-            'messages': [
-                {
-                    'type': 'text',
-                    'text': 'Hello, how may I help you today?',
-                    'timestamp': datetime.now()
-                }
-            ],
-            # 'actionClick': self.action_click,
-            # 'toolbarClick': self.toolbar_click,
-            # 'sendMessage': self.send_message,
-            # 'typing': self.typing,
-            # 'typingEnd': self.typing_end,
-            'post': self.chat_post,
-            # 'postEnd': self.post_end,
-            # 'typingIndicator': self.typing_indicator,
-            # 'typingIndicatorEnd': self.typing_indicator_end,
-            # 'scrollToBottom': self.scroll_to_bottom,
-            # 'scrollToBottomEnd': self.scroll_to_bottom_end,
-            # 'attachmentUpload': self.attachment_upload,
-            # 'attachmentUploadEnd': self.attachment_upload_end,
-            'typingIndicatorTimeout': 5000,
-            'toolbar': {
-                'toggleable': True,
-                'tools': [
-                    {
-                        'name': 'Insert',
-                        'iconClass': 'k-icon k-i-plus-outline'
-                    },
-                    {
-                        'name': 'Bold',
-                        'iconClass': 'k-icon k-i-bold'
-                    },
-                    {
-                        'name': 'Italic',
-                        'iconClass': 'k-icon k-i-italic'
-                    },
-                    {
-                        'name': 'Underline',
-                        'iconClass': 'k-icon k-i-underline'
-                    },
-                    {
-                        'name': 'strikethrough',
-                        'iconClass': 'k-icon k-i-strikethrough'
-                    },
-                    {
-                        'name': 'InsertUnorderedList',
-                        'iconClass': 'k-icon k-i-bullets'
-                    },
-                    {
-                        'name': 'InsertOrderedList',
-                        'iconClass': 'k-icon k-i-numbered-list'
-                    },
-                    {
-                        'name': 'Outdent',
-                        'iconClass': 'k-icon k-i-outdent'
-                    },
-                    {
-                        'name': 'Indent',
-                        'iconClass': 'k-icon k-i-indent'
-                    },
-                    {
-                        'name': 'CreateLink',
-                        'iconClass': 'k-icon k-i-hyperlink-create'
-                    },
-                    {
-                        'name': 'Unlink',
-                        'iconClass':
-                            'k-icon k-i-hyperlink-remove'
-                    },
-                    {
-                        'name': 'InsertImage',
-                        'iconClass': 'k-icon k-i-image-insert'
-                    },
-                    {
-                        'name': 'InsertFile',
-                        'iconClass': 'k-icon k-i-file-insert'
-                    },
-                    {
-                        'name': 'ViewHtml',
-                        'iconClass': 'k-icon k-i-html-view'
-                    },
-                    {
-                        'name': 'CleanFormatting',
-                        'iconClass': 'k-icon k-i-eraser'
-                    },
-                    {
-                        'name': 'Print',
-                        'iconClass': 'k-icon k-i-print'
-                    },
-                    {
-                        'name': 'Undo',
-                        'iconClass': 'k-icon k-i-undo'
-                    },
-                    {
-                        'name': 'Redo',
-                        'iconClass': 'k-icon k-i-redo'
-                    }
-                ]
-            },
-            'sendButton': {
-                'iconClass': 'k-icon k-i-send'
-            },
-            'placeholder': 'Type a message...',
-            'minLength': 1,
-            'attachments': {
-                'multiple': True,
-                'maxFileSize': 3145728
-            },
-            'width': '100%',
-            'height': '95%',
-            'scrollToBottomOnFocus': True,
-            'autoFocus': True,
-            'typingIndicatorOffset': 0,
-            'typingIndicatorHoldTime': 500,
-            'typingIndicatorScrollSpeed': 1000,
-            'typingIndicatorScrollDelay': 1000,
-            'typingIndicatorScrollDistance': 100,
-            'typingIndicatorOpacity': 0.5,
-            'typingIndicatorTypingDelay': 1000,
-            'typingIndicatorFadeDelay': 1000,
-            'typingIndicatorFadeOutDelay': 1000,
-            'typingIndicatorTemplate': '<span class="k-i-loading"></span>',
-            'messageTemplate': '<div class="k-message"><div class="k-message-wrapper"><div class="k-message-avatar"><img src="#= user.iconUrl #" alt="#= user.name # avatar" /></div><div class="k-message-content"><div class="k-message-author">#: user.name #</div><div class="k-message-text">#: text #</div></div></div></div>',
-            'systemMessageTemplate': '<div class="k-message"><div class="k-message-wrapper"><div class="k-message-content"><div class="k-message-text">#: text #</div></div></div></div>',
-        }).data('kendoChat')
+        # self.chat = jQuery(f"#{self.chat_id}").kendoChat({
+        #     'user': {
+        #         'name': 'User',
+        #         'iconUrl': 'https://demos.telerik.com/kendo-ui/content/chat/agent-01.png'
+        #     },
+        #     'messages': [
+        #         {
+        #             'type': 'text',
+        #             'text': 'Hello, how may I help you today?',
+        #             'timestamp': datetime.now()
+        #         }
+        #     ],
+        #     # 'actionClick': self.action_click,
+        #     # 'toolbarClick': self.toolbar_click,
+        #     # 'sendMessage': self.send_message,
+        #     # 'typing': self.typing,
+        #     # 'typingEnd': self.typing_end,
+        #     'post': self.chat_post,
+        #     # 'postEnd': self.post_end,
+        #     # 'typingIndicator': self.typing_indicator,
+        #     # 'typingIndicatorEnd': self.typing_indicator_end,
+        #     # 'scrollToBottom': self.scroll_to_bottom,
+        #     # 'scrollToBottomEnd': self.scroll_to_bottom_end,
+        #     # 'attachmentUpload': self.attachment_upload,
+        #     # 'attachmentUploadEnd': self.attachment_upload_end,
+        #     'typingIndicatorTimeout': 5000,
+        #     'toolbar': {
+        #         'toggleable': True,
+        #         'tools': [
+        #             {
+        #                 'name': 'Insert',
+        #                 'iconClass': 'k-icon k-i-plus-outline'
+        #             },
+        #             {
+        #                 'name': 'Bold',
+        #                 'iconClass': 'k-icon k-i-bold'
+        #             },
+        #             {
+        #                 'name': 'Italic',
+        #                 'iconClass': 'k-icon k-i-italic'
+        #             },
+        #             {
+        #                 'name': 'Underline',
+        #                 'iconClass': 'k-icon k-i-underline'
+        #             },
+        #             {
+        #                 'name': 'strikethrough',
+        #                 'iconClass': 'k-icon k-i-strikethrough'
+        #             },
+        #             {
+        #                 'name': 'InsertUnorderedList',
+        #                 'iconClass': 'k-icon k-i-bullets'
+        #             },
+        #             {
+        #                 'name': 'InsertOrderedList',
+        #                 'iconClass': 'k-icon k-i-numbered-list'
+        #             },
+        #             {
+        #                 'name': 'Outdent',
+        #                 'iconClass': 'k-icon k-i-outdent'
+        #             },
+        #             {
+        #                 'name': 'Indent',
+        #                 'iconClass': 'k-icon k-i-indent'
+        #             },
+        #             {
+        #                 'name': 'CreateLink',
+        #                 'iconClass': 'k-icon k-i-hyperlink-create'
+        #             },
+        #             {
+        #                 'name': 'Unlink',
+        #                 'iconClass':
+        #                     'k-icon k-i-hyperlink-remove'
+        #             },
+        #             {
+        #                 'name': 'InsertImage',
+        #                 'iconClass': 'k-icon k-i-image-insert'
+        #             },
+        #             {
+        #                 'name': 'InsertFile',
+        #                 'iconClass': 'k-icon k-i-file-insert'
+        #             },
+        #             {
+        #                 'name': 'ViewHtml',
+        #                 'iconClass': 'k-icon k-i-html-view'
+        #             },
+        #             {
+        #                 'name': 'CleanFormatting',
+        #                 'iconClass': 'k-icon k-i-eraser'
+        #             },
+        #             {
+        #                 'name': 'Print',
+        #                 'iconClass': 'k-icon k-i-print'
+        #             },
+        #             {
+        #                 'name': 'Undo',
+        #                 'iconClass': 'k-icon k-i-undo'
+        #             },
+        #             {
+        #                 'name': 'Redo',
+        #                 'iconClass': 'k-icon k-i-redo'
+        #             }
+        #         ]
+        #     },
+        #     'sendButton': {
+        #         'iconClass': 'k-icon k-i-send'
+        #     },
+        #     'placeholder': 'Type a message...',
+        #     'minLength': 1,
+        #     'attachments': {
+        #         'multiple': True,
+        #         'maxFileSize': 3145728
+        #     },
+        #     'width': '100%',
+        #     'height': '95%',
+        #     'scrollToBottomOnFocus': True,
+        #     'autoFocus': True,
+        #     'typingIndicatorOffset': 0,
+        #     'typingIndicatorHoldTime': 500,
+        #     'typingIndicatorScrollSpeed': 1000,
+        #     'typingIndicatorScrollDelay': 1000,
+        #     'typingIndicatorScrollDistance': 100,
+        #     'typingIndicatorOpacity': 0.5,
+        #     'typingIndicatorTypingDelay': 1000,
+        #     'typingIndicatorFadeDelay': 1000,
+        #     'typingIndicatorFadeOutDelay': 1000,
+        #     'typingIndicatorTemplate': '<span class="k-i-loading"></span>',
+        #     'messageTemplate': '<div class="k-message"><div class="k-message-wrapper"><div class="k-message-avatar"><img src="#= user.iconUrl #" alt="#= user.name # avatar" /></div><div class="k-message-content"><div class="k-message-author">#: user.name #</div><div class="k-message-text">#: text #</div></div></div></div>',
+        #     'systemMessageTemplate': '<div class="k-message"><div class="k-message-wrapper"><div class="k-message-content"><div class="k-message-text">#: text #</div></div></div></div>',
+        # }).data('kendoChat')
+
         self.chat = jQuery(f"#{self.chat_id}").kendoChat({
             'post': self.chat_post,
         }).data('kendoChat')
