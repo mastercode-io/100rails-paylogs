ee9c26d72e442a4326633f6d2be90c99fb41cb7f
Alex Sherin <al@mastercode.io>
2023-11-25 23:04:51 +0000

    Alex remote

--- client_code/app/HomePage/navigation.py
+++ client_code/app/HomePage/navigation.py
@@ -237,9 +237,9 @@ class Assistant:
     def sidebar_event(self, args):
         print('sidebar_event', args)
         if self.open:
-            self.control.hide()
-        else:
             self.control.show()
+        else:
+            self.control.hide()
 
 
 # Sidebar navigation class
