f560cda41d47d92c75f5b5bfa274aa4be38260af
Alex Sherin <al@mastercode.io>
2023-11-27 18:07:49 +0000

    Alex remote

--- server_code/copilot/__init__.py
+++ server_code/copilot/__init__.py
@@ -10,8 +10,9 @@ for attr in attributes:
 
 @anvil.server.callable
 def get_openai_client(api_key=None):
-    client = openai.Client(api_key=api_key)
-    return client
+    # client = openai.Client(api_key=api_key)
+    # return client
+    return None
 
 
 @anvil.server.callable
