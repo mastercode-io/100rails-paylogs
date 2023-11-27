bf6415c94c414965827f97c740cfa659174dcd3f
Alex Sherin <al@mastercode.io>
2023-11-27 16:56:57 +0000

    Alex remote

--- server_code/copilot/__init__.py
+++ server_code/copilot/__init__.py
@@ -2,11 +2,11 @@ import anvil.server
 # from openai import OpenAI
 
 
-def get_openai_client(api_key=None):
-    pass
-    # client = OpenAI(api_key=api_key)
-    # return client
-
-
-def get_current_thread_id():
-    return anvil.server.session.get('current_thread_id', None)
+# def get_openai_client(api_key=None):
+#     pass
+#     # client = OpenAI(api_key=api_key)
+#     # return client
+#
+#
+# def get_current_thread_id():
+#     return anvil.server.session.get('current_thread_id', None)
