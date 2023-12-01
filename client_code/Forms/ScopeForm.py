50eab39b7d82336c4a6feefc48cf057f9cdf43b4
Alex Sherin <al@mastercode.io>
2023-12-01 15:50:23 +0000

    Alex remote

--- client_code/Forms/ScopeForm.py
+++ client_code/Forms/ScopeForm.py
@@ -1,6 +1,7 @@
 from AnvilFusion.components.FormBase import FormBase
 from AnvilFusion.components.FormInputs import *
 from AnvilFusion.components.MultiFieldInput import MultiFieldInput
+from ..app.models import JobType
 
 
 class ScopeForm(FormBase):
@@ -9,13 +10,16 @@ class ScopeForm(FormBase):
         kwargs['model'] = 'Scope'
 
         self.name = TextInput(name='name', label='Name')
+        self.short_code = DropdownInput(name='short_code', label='Short Code', enabled=False)
         self.description = MultiLineInput(name='description', label='Description', rows=4)
-        self.type = LookupInput(name='type', label='Scope Type', model='ScopeType')
+        self.type = LookupInput(name='type', label='Scope Type', model='ScopeType',
+                                on_chenge=self.scope_type_on_change)
         self.status = RadioButtonInput(name='status', label='Status', options=['Active', 'Inactive'], value='Active')
         # self.custom_fields = MultiFieldInput(name='custom_fields', label='Custom Fields', model='Scope', cols=2)
 
         fields = [
             self.name,
+            self.short_code,
             self.description,
             self.type,
             self.status,
@@ -23,3 +27,10 @@ class ScopeForm(FormBase):
 
         super().__init__(fields=fields, **kwargs)
         # self.fullscreen = True
+
+
+    def scope_type_on_change(self, args):
+        if self.type.value is None or args.get('value', None) is None:
+            self.short_code.options = []
+        elif self.type.value == 'Job Type':
+            self.short_code.options = [j['short_code'] for j in JobType.search()]
