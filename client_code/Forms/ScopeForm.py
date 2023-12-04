4a0400a35fd652fdbb31f1fa97beeb823a032d9b
Alex Sherin <al@mastercode.io>
2023-12-04 15:59:43 +0000

    Alex remote

--- client_code/Forms/ScopeForm.py
+++ client_code/Forms/ScopeForm.py
@@ -34,7 +34,6 @@ class ScopeForm(FormBase):
         if self.type.value is None or args.get('value', None) is None:
             self.short_code.options = []
         elif self.type.value['name'] == 'Job Type':
-            options = [j['short_code'] for j in JobType.search()]
-            print('job type options', options)
-            self.short_code.options = options
-            print('short_code options', self.short_code.options)
+            self.short_code.options = [j['short_code'] for j in JobType.search()]
+        else:
+            self.short_code.options = []
