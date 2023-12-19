d28b300f7be9364c3adeb1c37d73ae8ed9bf986c
alexsherin <alex@100email.co>
2023-12-19 16:25:10 +0000

    Alex dev commit

--- client_code/payroll/pay_awards.py
+++ client_code/payroll/pay_awards.py
@@ -66,16 +66,16 @@ class PyaRateRuleAward(PayRateRule):
                     unallocated_time.append((allocated_end_time, end_time))
                 else:
                     allocated_end_time = end_time
-                if 'OT150% WD' in self.name:
+                if 'OT150% WD' in self.name or 'OT200% WD' in self.name:
                     print(self.name, allocated_start_time, allocated_end_time)
                 units = (allocated_end_time - allocated_start_time).total_seconds() / 3600
-                if 'OT150% WD' in self.name:
+                if 'OT150% WD' in self.name or 'OT200% WD' in self.name:
                     print(self.name, units)
                 if self.max_hours and units > self.max_hours:
                     units = self.max_hours
                     unallocated_time.append(
                         (allocated_start_time + timedelta(hours=self.max_hours), allocated_end_time))
-                    if 'OT150% WD' in self.name:
+                    if 'OT150% WD' in self.name or 'OT200% WD' in self.name:
                         print(self.name, units, unallocated_time)
         else:
             unallocated_time.append((start_time, end_time))
