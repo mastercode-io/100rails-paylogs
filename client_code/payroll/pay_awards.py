b8b2f7a35069920085c63d083550502496e1a52c
alexsherin <alex@100email.co>
2023-12-19 15:31:49 +0000

    Alex dev commit

--- client_code/payroll/pay_awards.py
+++ client_code/payroll/pay_awards.py
@@ -64,11 +64,14 @@ class PyaRateRuleAward(PayRateRule):
                 else:
                     allocated_end_time = end_time
                 units = (allocated_end_time - allocated_start_time).total_seconds() / 3600
-                print(self.name, units)
+                if day_type(date)[1] == 'Weekend':
+                    print(self.name, units)
                 if self.time_limits and units > self.max_hours:
                     units = self.max_hours
                     unallocated_time.append(
                         (allocated_start_time + timedelta(hours=self.max_hours), allocated_end_time))
+                    if day_type(date)[1] == 'Weekend':
+                        print(self.name, units, unallocated_time)
         else:
             unallocated_time.append((start_time, end_time))
         # print(self.name, units, unallocated_time)
