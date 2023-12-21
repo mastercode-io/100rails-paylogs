35e1e58ce3ba6cac74ea2d2f2c13d58e11f9793c
alexsherin <alex@100email.co>
2023-12-21 19:26:34 +0000

    Alex dev commit

--- server_code/api/__init__.py
+++ server_code/api/__init__.py
@@ -77,10 +77,10 @@ def decode_tenant_api_key(tenant_uid, api_key):
 
 
 def authenticate_request(request: anvil.server.request):
-    tenant_uid = request.headers.get('X-Tenant-UID', None)
-    api_key = request.headers.get('X-API-Key', None)
+    tenant_uid = request.headers.get('x-tenant-uid', None)
+    api_key = request.headers.get('x-api-key', None)
     if not tenant_uid or not api_key:
-        return False, f'Missing X-Tenant-UID or X-API-Key header: {request.headers}'
+        return False, f'Missing x-tenant-uid or x-api-key header: {request.headers}'
     else:
         api_user_name, api_user_password = decode_tenant_api_key(tenant_uid, api_key)
         api_user_email = get_api_user_email(tenant_uid, api_user_name)
