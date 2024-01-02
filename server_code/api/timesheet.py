7c7c820771d660dc277678bbcdac6d733b25f579
alexsherin <alex@100email.co>
2024-01-02 14:21:24 +0000

    Alex dev commit

--- server_code/api/timesheet.py
+++ server_code/api/timesheet.py
@@ -6,12 +6,12 @@ import datetime
 
 
 @anvil.server.http_endpoint("/timesheets", methods=["GET", "POST"])
-def timesheet_endpoint(timesheet_uid, **params):
+def timesheet_endpoint(**params):
     auth, message = authenticate_request(anvil.server.request)
     if not auth:
         return anvil.server.HttpResponse(401, message)
     return (f"method: {anvil.server.request.method}, headers: {anvil.server.request.headers}\n"
-            f"timesheet_uid: {timesheet_uid}, params: {params}\n"
+            # f"timesheet_uid: {timesheet_uid}, params: {params}\n"
             f"body: {anvil.server.request.body_json}\n")
     # if anvil.server.request.method == "GET":
     #     timesheet = Timesheet.get(timesheet_uid)
