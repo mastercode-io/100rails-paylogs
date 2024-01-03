2a5eacf88f2de00f04181b4463b9083c7ec65d18
alexsherin <alex@100email.co>
2024-01-03 16:00:55 +0000

    Alex dev commit

--- client_code/@execute.py
+++ client_code/@execute.py
@@ -44,4 +44,4 @@ init_user_session()
 # tenant_uid = 'a48a5f3f-f4a0-40a7-9b56-23d08c98e182'
 # api_credential = anvil.server.call('generate_api_key', tenant_uid, api_service)
 # print(api_credential['api_key'])
-anvil.server.call('foo')
+# anvil.server.call('foo')
