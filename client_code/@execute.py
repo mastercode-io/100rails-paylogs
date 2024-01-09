4b46b26de9540b4ada0a6ac6703ca3eea0a9d2aa
alexsherin <alex@100email.co>
2024-01-09 15:14:50 +0000

    Alex dev commit

--- client_code/@execute.py
+++ client_code/@execute.py
@@ -33,10 +33,7 @@ def add_enum_list():
 
 
 init_user_session()
-timesheet_schema = models.Timesheet.get_json_schema()
-# print(timesheet_schema)
-
-ts = models.Timesheet.get('febd487a-f5ab-45f5-862a-8a1016568d27')
+emp = models.Employee.get('56660563-c20d-4c2d-9a0b-361e5f660d8d')
 # print(ts.to_json_dict(json_schema=timesheet_schema))
 
 # api_service = api.register_api_service(
