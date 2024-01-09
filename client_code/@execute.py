8c729f0c8b7364992fe282c067ec75f2105eb629
alexsherin <alex@100email.co>
2024-01-09 17:53:20 +0000

    Alex dev commit

--- client_code/@execute.py
+++ client_code/@execute.py
@@ -38,7 +38,8 @@ emp = ts['employee']
 # emp = models.Employee.get('56660563-c20d-4c2d-9a0b-361e5f660d8d')
 print(emp)
 ts = models.Timesheet.search(employee=emp)
-print(len(ts))
+print(len(ts), ts.page, ts.total_pages, ts.rows_id)
+print([*ts])
 for t in ts:
     print(t)
 # print(ts.to_json_dict(json_schema=timesheet_schema))
