71e478c07cd9a8af2cd5b2c65b1c0cb99d3a4e57
alexsherin <alex@100email.co>
2024-01-09 16:04:55 +0000

    Alex dev commit

--- client_code/@execute.py
+++ client_code/@execute.py
@@ -37,9 +37,8 @@ ts = models.Timesheet.get('febd487a-f5ab-45f5-862a-8a1016568d27')
 emp = ts['employee']
 # emp = models.Employee.get('56660563-c20d-4c2d-9a0b-361e5f660d8d')
 print(emp)
-ts = models.Timesheet.search(employee=emp)
-for t in ts:
-    print(t)
+ts = models.Timesheet.search()
+print(len(ts))
 # print(ts.to_json_dict(json_schema=timesheet_schema))
 
 # api_service = api.register_api_service(
