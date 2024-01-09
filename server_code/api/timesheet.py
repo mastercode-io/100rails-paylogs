0f9ca5f6a43ceb2f50ae1a1dee74a1ea8e077e63
alexsherin <alex@100email.co>
2024-01-09 14:48:09 +0000

    Alex dev commit

--- server_code/api/timesheet.py
+++ server_code/api/timesheet.py
@@ -1,6 +1,8 @@
-from ..app.models import Timesheet
+from ..app.models import Timesheet, Employee
 from .. import api
 import anvil.server
+from anvil.tables import query as q
+import anvil.tables as tables
 import json
 import datetime
 
@@ -71,10 +73,44 @@ def test_endpoint(timesheet_uid, **params):
             else:
                 return anvil.server.HttpResponse(404, f"Timesheet not found: {timesheet_uid}")
         else:
-            page = int(params.get('page', 1))
-            timesheets = Timesheet.search(page=page, page_length=TIMESHEET_PAGE_LENGTH)
-            print('timesheets search result:')
-            print(timesheets.count, timesheets.total_pages, timesheets.page_length, timesheets.page)
+            page = params.get('page', 1)
+            start_date = params.get('start_date', '')
+            end_date = params.get('end_date', '')
+            employee_uid = params.get('employee_uid', None)
+            employee_link_id = params.get('employee_link_id', None)
+            try:
+                page = int(page)
+            except ValueError:
+                page = 1
+            try:
+                start_date = datetime.datetime.strptime(start_date, '%Y-%m-%d').date()
+            except (ValueError, TypeError):
+                start_date = None
+            try:
+                end_date = datetime.datetime.strptime(end_date, '%Y-%m-%d').date()
+            except (ValueError, TypeError):
+                end_date = None
+            filters = {}
+            queries = []
+            if employee_uid:
+                employee = Employee.get(employee_uid)
+                if employee:
+                    filters['employee'] = employee
+            elif employee_link_id:
+                filters['remote_links'] = {integration_name: employee_link_id}
+            if start_date and not end_date:
+                filters['date'] = q.greater_than_or_equal_to(start_date)
+            elif end_date and not start_date:
+                filters['date'] = q.less_than_or_equal_to(end_date)
+            elif start_date and end_date:
+                filters['date'] = q.between(start_date, end_date, max_inclusive=True)
+            filters['search_query'] = [
+                tables.order_by('employee'),
+                tables.order_by('date'),
+            ]
+            timesheets = Timesheet.search(page=page, page_length=TIMESHEET_PAGE_LENGTH, **filters)
+            # print('timesheets search result:')
+            # print(timesheets.count, timesheets.total_pages, timesheets.page_length, timesheets.page)
             ts_list = [ts.to_json_dict(json_schema=TIMESHEET_JSON_SCHEMA) for ts in timesheets]
             return anvil.server.HttpResponse(
                 200,
