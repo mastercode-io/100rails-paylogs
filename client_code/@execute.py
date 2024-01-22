a834fe7444cdd821a498e5e732d70926272addb5
alexsherin <alex@100email.co>
2024-01-22 18:34:27 +0000

    Alex dev commit

--- client_code/@execute.py
+++ client_code/@execute.py
@@ -60,4 +60,4 @@ init_user_session()
 # items = models.JobType.search(remote_links={'76e14124-04dc-49e4-9f18-27b25a66f67e': 1886330000100840400})
 items = models.JobType.search()
 for item in items:
-    print('job type', item['name'], item['remote_links'])
+    print('job type', item['name'], item['remote_links'], type(item['remote_links']))
