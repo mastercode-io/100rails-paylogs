33651a53b5c2f2965efd48cc67ac97b63d5f68f2
alexsherin <alex@100email.co>
2024-01-22 18:51:10 +0000

    Alex dev commit

--- server_code/api/__init__.py
+++ server_code/api/__init__.py
@@ -319,7 +319,7 @@ def post_item(resource, post_data, integration):
                     if len(rel_item) == 0:
                         post_data['error'] = f"ValidationError, {relationship} not found: {rel_json['link_id']}"
                         return post_data, {'status': 400, 'error': post_data['error']}
-                    item_data[relationship] = {'uid': next(rel_item)['uid']}
+                    item_data[relationship] = {'uid': rel_item[0]['uid']}
     if 'link_id' in post_data and 'remote_links' in resource_class._attributes:
         if not item.get('remote_links', None):
             item_data['remote_links'] = {'dummy': 'dummy'}
