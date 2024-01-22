0140874271e8acb8313eb35b7b2a8230fe6d45de
alexsherin <alex@100email.co>
2024-01-22 18:41:16 +0000

    Alex dev commit

--- server_code/api/__init__.py
+++ server_code/api/__init__.py
@@ -312,14 +312,14 @@ def post_item(resource, post_data, integration):
                 if 'uid' in rel_json:
                     item_data[relationship] = {'uid': rel_json['uid']}
                 elif 'link_id' in rel_json:
-                    rel_item = resource_class._relationships[relationship].cls.get_by(
+                    rel_item = resource_class._relationships[relationship].cls.search(
                         'remote_links', {integration['uid']: rel_json['link_id']}
                     )
                     print('rel_item', rel_item, 'remote_links', {integration['uid']: rel_json['link_id']})
                     if not rel_item:
                         post_data['error'] = f"ValidationError, {relationship} not found: {rel_json['link_id']}"
                         return post_data, {'status': 400, 'error': post_data['error']}
-                    item_data[relationship] = {'uid': rel_item['uid']}
+                    item_data[relationship] = {'uid': rel_item[0]['uid']}
     if 'link_id' in post_data and 'remote_links' in resource_class._attributes:
         if not item.get('remote_links', None):
             item_data['remote_links'] = {'dummy': 'dummy'}
