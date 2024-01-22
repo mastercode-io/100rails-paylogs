626280f30e9dcc3e1f6cfed446cb7f42701c6afa
alexsherin <alex@100email.co>
2024-01-22 17:58:26 +0000

    Alex dev commit

--- client_code/@execute.py
+++ client_code/@execute.py
@@ -57,6 +57,6 @@ init_user_session()
 # api_credential = anvil.server.call('generate_api_key', tenant_uid, api_service)
 # print(api_credential['api_key'])
 # anvil.server.call('foo')
-items = models.JobType.search(remote_links={'76e14124-04dc-49e4-9f18-27b25a66f67e': 1886330000121072400})
+items = models.JobType.search(remote_links={'76e14124-04dc-49e4-9f18-27b25a66f67e': 1886330000100840400})
 for item in items:
     print('job type', item)
