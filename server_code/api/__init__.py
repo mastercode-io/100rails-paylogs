5a1b67599298949ffa4f05e6769687df85ff9c25
alexsherin <alex@100email.co>
2024-01-26 19:15:26 +0000

    Alex dev commit

--- server_code/api/__init__.py
+++ server_code/api/__init__.py
@@ -350,8 +350,8 @@ def set_timeout(num_seconds):
 @anvil.server.http_endpoint("/timeout", methods=["GET", "POST"])
 def long_running_function():
     try:
-        set_timeout(28)
-        print(f"Alarm set for: 28 seconds")
+        # set_timeout(28)
+        # print(f"Alarm set for: 28 seconds")
         for i in range(33):
             print(f"i: {i}")
             time.sleep(1)
@@ -361,7 +361,7 @@ def long_running_function():
             {'content-type': 'application/json'},
         )
     finally:
-        signal.alarm(0)
+        # signal.alarm(0)
         return anvil.server.HttpResponse(
             202,
             json.dumps({'status': 'incomplete', 'message': 'Request timed out. Use bulk API to process this request.'}),
