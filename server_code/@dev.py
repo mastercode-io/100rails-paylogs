38e1fdac0207b403a3a8f80c28c84a1f4950422c
alexsherin <alex@100email.co>
2024-01-27 14:20:05 +0000

    Alex dev commit

--- server_code/@dev.py
+++ server_code/@dev.py
@@ -6,4 +6,12 @@ from AnvilFusion.server.utils import get_logged_user
 
 @anvil.server.callable
 def foo():
-    print('Logged user: ', get_logged_user())
+    print('Lunching BG task')
+    bg_task = anvil.server.launch_background_task('background_task')
+    print(bg_task)
+
+
+@anvil.server.background_task
+def background_task():
+    print('Background task: ', get_logged_user())
+    return 'Background task done'
