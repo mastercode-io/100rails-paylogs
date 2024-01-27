11dc1cd8da6f3292567c03f4c743c8a4c4957706
alexsherin <alex@100email.co>
2024-01-27 15:51:58 +0000

    Alex dev commit

--- server_code/@dev.py
+++ server_code/@dev.py
@@ -16,7 +16,7 @@ def background_task(logged_user=None):
     print('Background task started', logged_user)
     if logged_user:
         save_logged_user(current_user=logged_user)
-    anvil.server.session['background_task'] = 'running'
+    # anvil.server.session['background_task'] = 'running'
     # print('Background task session: ', anvil.server.session['logged_user'])
     # print('local function', bar())
     print('AnvilFusion function', get_logged_user())
