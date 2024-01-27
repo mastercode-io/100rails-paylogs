e9688150f0b12aac29f9fbf95aceed74b6da7bb8
alexsherin <alex@100email.co>
2024-01-27 14:15:44 +0000

    Alex dev commit

--- server_code/@dev.py
+++ server_code/@dev.py
@@ -1,8 +1,9 @@
 import anvil.server
 import anvil.users
 import uuid
+from AnvilFusion.server.utils import get_logged_user
 
 
 @anvil.server.callable
 def foo():
-    print('SERVER SESSION\n', anvil.server.session.keys())
+    print('Logged user: ', get_logged_user())
