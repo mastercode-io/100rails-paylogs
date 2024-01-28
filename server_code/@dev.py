7ad15b166a87b83bc1d33953ff1fa39bef68e4bc
alexsherin <alex@100email.co>
2024-01-28 18:12:36 +0000

    Alex dev commit

--- server_code/@dev.py
+++ server_code/@dev.py
@@ -56,7 +56,7 @@ def bar():
     print('bar context', anvil.server.context)
     print('BAR', get_logged_user())
     tenant = models.Tenant.search()
-    print('tenant', tenant, tenant.count, tenant.length)
+    print('tenant', tenant, tenant.count)
     for t in tenant:
         print(t)
     return 'bar'
