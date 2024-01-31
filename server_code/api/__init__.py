e0404ccbf3ab3aeb03cd7d033286c24679222063
alexsherin <alex@100email.co>
2024-01-31 17:59:21 +0000

    Alex dev commit

--- server_code/api/__init__.py
+++ server_code/api/__init__.py
@@ -389,8 +389,8 @@ def post_item(resource_name, resource_model, resource_json_schema, post_data, in
             item_data['remote_links'] = {}
         item_data['remote_links'][integration['uid']] = post_data['link_id']
     item.update(item_data)
-    print('item_data', item_data)
-    print(item['date'], item['start_time'], item['end_time'])
+    # print('item_data', item_data)
+    # print(item['date'], item['start_time'], item['end_time'])
     item.save()
     item = resource_class.get(item['uid'])
     item_json = item.to_json_dict(json_schema=resource_json_schema, integration_uid=integration['uid'])
