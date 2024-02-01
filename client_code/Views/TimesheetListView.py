6f832737ae333b17505bc6d3f00a6a2c7e4cec8e
alexsherin <alex@100email.co>
2024-02-01 21:31:56 +0000

    Alex dev commit

--- client_code/Views/TimesheetListView.py
+++ client_code/Views/TimesheetListView.py
@@ -80,18 +80,9 @@ class TimesheetListView(GridView):
                 f'{args.items[0].employee__full_name}</div>')
 
     def grouping_total_hours(self, data, column):
-        print('\n\ngrouping_total_hours\n\n', data)
         if isinstance(data, list):
             return
-        # week_total = sum(day['total_hours'] for day in data.items)
-        week_total = 0
-        for day in data.items:
-            print(day['total_hours'])
-            if day['total_hours']:
-                week_total += day['total_hours']
-            else:
-                print(type(day['total_hours']))
-        print('week_total', week_total)
+        week_total = sum(ts['total_hours'] for ts in data.items if ts['total_hours'])
         hours = int(week_total)
         minutes = int((week_total - hours) * 60)
         return f"{hours}:{minutes:02d} week"
