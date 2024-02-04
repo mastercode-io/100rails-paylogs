ba73bc4dea144d5aba9f491576a99763a7245a9d
alexsherin <alex@100email.co>
2024-02-04 18:44:12 +0000

    Alex dev commit

--- client_code/Forms/PayRateTemplateItemForm.py
+++ client_code/Forms/PayRateTemplateItemForm.py
@@ -9,20 +9,20 @@ class PayRateTemplateItemForm(FormBase):
         kwargs['model'] = 'PayRateTemplateItem'
 
         self.order_number = NumberInput(name='order_number', label='Order', format='##')
-        self.pay_rate_title = TextInput(name='pay_rate_title', label='Title')
+        self.default_pay_rate_title = TextInput(name='default_pay_rate_title', label='Default Title')
         self.pay_rate_rule = LookupInput(name='pay_rate_rule', label='Rule', model='PayRateRule',
                                          on_change=self.pay_rate_rule_selected)
-        self.pay_category = LookupInput(name='pay_category', label='Pay Category', model='PayCategory',
-                                        on_change=self.pay_category_selected)
-        self.pay_rate = NumberInput(name='pay_rate', label='Rate', format='c2')
+        self.default_pay_category = LookupInput(name='default_pay_category', label='Default Pay Category',
+                                                model='PayCategory', on_change=self.pay_category_selected)
+        self.default_pay_rate = NumberInput(name='default_pay_rate', label='Default Rate', format='c2')
         self.pay_rate_multiplier = NumberInput(name='pay_rate_multiplier', label='Multiplier', format='p2')
         self.status = RadioButtonInput(name='status', label='Status', options=['Active', 'Inactive'], value='Active')
 
         fields = [
             self.pay_rate_rule,
-            self.pay_category,
-            self.pay_rate_title,
-            self.pay_rate,
+            self.default_pay_category,
+            self.default_pay_rate_title,
+            self.default_pay_rate,
             self.pay_rate_multiplier,
             self.order_number,
             self.status,
@@ -30,22 +30,20 @@ class PayRateTemplateItemForm(FormBase):
 
         super().__init__(fields=fields, **kwargs)
 
-
     def pay_rate_rule_selected(self, args):
         print('pay_rate_rule_selected', self.pay_rate_rule.value, args)
         if self.pay_rate_rule.value is None or args.get('value', None) is None:
-            self.pay_rate.value = None
+            self.default_pay_rate.value = None
             self.pay_rate_multiplier.value = None
         else:
             pay_rate_rule = PayRateRule.get(self.pay_rate_rule.value['uid'])
-            self.pay_rate.value = pay_rate_rule['pay_rate']
+            self.default_pay_rate.value = pay_rate_rule['pay_rate']
             self.pay_rate_multiplier.value = pay_rate_rule['pay_rate_multiplier']
-            self.pay_rate_title.value = pay_rate_rule['name']
-
+            self.default_pay_rate_title.value = pay_rate_rule['name']
 
     def pay_category_selected(self, args):
-        print('pay_category_selected', self.pay_category.value, args)
-        if self.pay_category.value is None or args.get('value', None) is None:
-            self.pay_rate_title.value = None
+        print('pay_category_selected', self.default_pay_category.value, args)
+        if self.default_pay_category.value is None or args.get('value', None) is None:
+            self.default_pay_rate_title.value = None
         else:
-            self.pay_rate_title.value = self.pay_category.value['name']
+            self.default_pay_rate_title.value = self.default_pay_category.value['name']
