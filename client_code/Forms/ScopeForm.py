a8a6a605ead16beb59a21dac7a590808512562ef
alexsherin <alex@100email.co>
2024-02-04 15:03:09 +0000

    Alex dev commit

--- client_code/Forms/ScopeForm.py
+++ client_code/Forms/ScopeForm.py
@@ -27,10 +27,8 @@ class ScopeForm(FormBase):
 
         super().__init__(fields=fields, **kwargs)
         self.scope_type_on_change({})
-        print('ScopeForm', self.data, self.data['short_code'])
 
     def scope_type_on_change(self, args):
-        print('scope_type_on_change', self.type.value, args)
         if self.type.value is None or args.get('value', None) is None:
             self.short_code.options = []
         elif self.type.value['name'] == 'Job Type':
