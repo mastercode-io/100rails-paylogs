c2afeb087abb114238401ea19e1e434f5a232817
alexsherin <alex@100email.co>
2024-02-04 14:44:04 +0000

    Alex dev commit

--- client_code/Forms/ScopeForm.py
+++ client_code/Forms/ScopeForm.py
@@ -10,10 +10,10 @@ class ScopeForm(FormBase):
         kwargs['model'] = 'Scope'
 
         self.name = TextInput(name='name', label='Name')
-        self.short_code = DropdownInput(name='short_code', label='Short Code')
-        self.description = MultiLineInput(name='description', label='Description', rows=4)
         self.type = LookupInput(name='type', label='Scope Type', model='ScopeType',
                                 on_change=self.scope_type_on_change)
+        self.short_code = DropdownInput(name='short_code', label='Short Code', enabled=False)
+        self.description = MultiLineInput(name='description', label='Description', rows=4)
         self.status = RadioButtonInput(name='status', label='Status', options=['Active', 'Inactive'], value='Active')
         # self.custom_fields = MultiFieldInput(name='custom_fields', label='Custom Fields', model='Scope', cols=2)
 
@@ -28,7 +28,6 @@ class ScopeForm(FormBase):
         super().__init__(fields=fields, **kwargs)
         # self.fullscreen = True
 
-
     def scope_type_on_change(self, args):
         print('scope_type_on_change', self.type.value, args)
         if self.type.value is None or args.get('value', None) is None:
@@ -37,3 +36,4 @@ class ScopeForm(FormBase):
             self.short_code.options = [j['short_code'] for j in JobType.search()]
         else:
             self.short_code.options = []
+        self.short_code.enabled = True if self.short_code.options else False
