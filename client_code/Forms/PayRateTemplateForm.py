e0a3bf56d13546bb5bc4843c7089410d0beb9dda
alexsherin <alex@100email.co>
2024-02-20 16:53:40 +0000

    Alex dev commit

--- client_code/Forms/PayRateTemplateForm.py
+++ client_code/Forms/PayRateTemplateForm.py
@@ -46,6 +46,7 @@ class PayRateTemplateForm(FormBase):
             }
         ]
 
+        print(kwargs.get('data'))
         if kwargs and kwargs.get('data', {}).get('uid', None) is None:
             super().__init__(sections=sections,
                              width=POPUP_WIDTH_COL3,
