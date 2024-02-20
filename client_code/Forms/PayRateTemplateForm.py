70dc70a1e573e17cb0373c1da48d72c6d0eb52ed
alexsherin <alex@100email.co>
2024-02-20 16:50:08 +0000

    Alex dev commit

--- client_code/Forms/PayRateTemplateForm.py
+++ client_code/Forms/PayRateTemplateForm.py
@@ -46,7 +46,7 @@ class PayRateTemplateForm(FormBase):
             }
         ]
 
-        if kwargs.get('data', {}).get('uid', None) is None:
+        if kwargs and kwargs.get('data', {}).get('uid', None) is None:
             super().__init__(sections=sections,
                              width=POPUP_WIDTH_COL3,
                              header='Crete Pay Rate Template',
