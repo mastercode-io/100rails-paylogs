d385e08221bb21ff895a72ed7388eac68a929476
alexsherin <alex@100email.co>
2024-02-20 16:18:58 +0000

    Alex dev commit

--- client_code/Forms/PayRateTemplateForm.py
+++ client_code/Forms/PayRateTemplateForm.py
@@ -56,5 +56,5 @@ class PayRateTemplateForm(FormBase):
             self.items.hide()
         print('buttons', self.form.buttons)
         button0 = self.form.buttons[0]
-        print(button0.isPrimary, button0.content, button0.cssClass)
+        print(button0.buttonModel.isPrimary, button0.buttonModel.content, button0.buttonModel.cssClass)
         print(self.form.header)
