7d4fbe55102a58ba4e4186abb41ccdd2103bea2e
alexsherin <alex@100email.co>
2024-02-21 15:18:50 +0000

    Alex dev commit

--- client_code/Forms/PayRateTemplateForm.py
+++ client_code/Forms/PayRateTemplateForm.py
@@ -81,6 +81,7 @@ class PayRateTemplateForm(FormBase):
             self.fullscreen = True
             self.form.header = 'Edit Pay Rate Template'
             save_button = self.form_el.querySelector('.da-save-button')
+            print('save_button', self.form_el, save_button)
             save_button.innerHTML = 'Save'
             # for button in self.form.buttons:
             #     if button.buttonModel.cssClass == 'da-save-button':
