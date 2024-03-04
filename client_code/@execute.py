369764cb33cb179775f101d632bb3b391b24d3a4
alexsherin <alex@100email.co>
2024-03-04 17:21:33 +0000

    Alex dev commit

--- client_code/@execute.py
+++ client_code/@execute.py
@@ -198,8 +198,8 @@ OTPH200_RULE = 'OT200% PH'
 pay_rate_template = models.PayRateTemplate.get_by('name', 'T2 Job')
 print(pay_rate_template)
 rate_item = [*models.PayRateTemplateItem.search(pay_rate_template=pay_rate_template,
-                                                default_pay_rate_title=OT150WD_RULE)]
-print(rate_item, OT150WD_RULE)
+                                                default_pay_rate_title=OTSAT150_RULE)]
+print(rate_item, OTSAT150_RULE)
 for rate_name in mult15_rates:
     if 'SUP' in rate_name:
         role_name = rate_name[0:4] + ' SALARY'
