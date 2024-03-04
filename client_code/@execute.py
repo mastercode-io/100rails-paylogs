cf072e92c573782272f76b59e8b121d150b56893
alexsherin <alex@100email.co>
2024-03-04 15:04:34 +0000

    Alex dev commit

--- client_code/@execute.py
+++ client_code/@execute.py
@@ -154,7 +154,7 @@ pay_rate_template = models.PayRateTemplate.get_by('name', 'T1 Job')
 print(pay_rate_template)
 rate_item = [*models.PayRateTemplateItem.search(pay_rate_template=pay_rate_template,
                                                 default_pay_rate_title=ORD_RULE)]
-print(rate_item, OTPH200_RULE)
+print(rate_item, ORD_RULE)
 for rate_name in ord_rates:
     if 'SUP' in rate_name:
         role_name = rate_name[0:4] + ' SALARY'
