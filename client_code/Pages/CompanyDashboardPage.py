384f8205362d287674ce4dab7a250987be5fd333
alexsherin <alex@100email.co>
2024-03-12 21:44:58 +0000

    Alex dev commit

--- client_code/Pages/CompanyDashboardPage.py
+++ client_code/Pages/CompanyDashboardPage.py
@@ -1,7 +1,7 @@
 from AnvilFusion.components.DashboardPage import DashboardPage
 
-# PANEL_CSS_CLASS = 'pl-company-dashboard-panel'
-PANEL_CSS_CLASS = ''
+PANEL_CSS_CLASS = 'pl-company-dashboard-panel'
+# PANEL_CSS_CLASS = ''
 
 
 class CompanyDashboardPage(DashboardPage):
@@ -17,25 +17,25 @@ class CompanyDashboardPage(DashboardPage):
                 {
                     'sizeX': 1, 'sizeY': 1, 'row': 0, 'col': 0,
                     'id': 'widget_a',
-                    'header': 'Widget A',
+                    # 'header': 'Widget A',
                     'cssClass': PANEL_CSS_CLASS,
                 },
                 {
                     'sizeX': 1, 'sizeY': 1, 'row': 0, 'col': 0,
                     'id': 'widget_b',
-                    'header': 'Widget B',
+                    # 'header': 'Widget B',
                     'cssClass': PANEL_CSS_CLASS,
                 },
                 {
                     'sizeX': 2, 'sizeY': 1, 'row': 1, 'col': 0,
                     'id': 'widget_c',
-                    'header': 'Widget C',
+                    # 'header': 'Widget C',
                     'cssClass': PANEL_CSS_CLASS,
                 },
                 {
                     'sizeX': 2, 'sizeY': 2, 'row': 0, 'col': 2,
                     'id': 'widget_d',
-                    'header': 'Widget D',
+                    # 'header': 'Widget D',
                     'cssClass': PANEL_CSS_CLASS,
                 },
             ],
