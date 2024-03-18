a52d4849767483350254ead4d2549dc0d7b0f115
alexsherin <alex@100email.co>
2024-03-18 18:24:41 +0000

    Alex dev commit

--- client_code/Pages/CompanyDashboardPage.py
+++ client_code/Pages/CompanyDashboardPage.py
@@ -14,7 +14,6 @@ class CompanyDashboardPage(DashboardPage):
                 height:100%; text-align:center; display: flex; flex-direction: column; justify-content: center;">\
                     <h4>Total Staff This Pay</h4>\
                     <h1>1,400</h1>\
-                    <br>\
                     <p style="font-size: 16px;color: green;">\
                         <i class="fa-solid fa-triangle" style="font-size: 30px;"></i>\
                         &nbsp;&nbsp;&nbsp;Up by 3\
