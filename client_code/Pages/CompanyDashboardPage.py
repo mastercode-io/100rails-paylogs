3fadbb5d075a93e40fcf331d341ea9e3b88b8ee3
alexsherin <alex@100email.co>
2024-03-18 18:33:32 +0000

    Alex dev commit

--- client_code/Pages/CompanyDashboardPage.py
+++ client_code/Pages/CompanyDashboardPage.py
@@ -10,11 +10,15 @@ class CompanyDashboardPage(DashboardPage):
 
         total_staff_widget_content = f'\
             <div style="padding: 10px; height:100%;">\
-                <div height="100%" style="border: 1px solid; border-radius: 5px; background-color:#F5F5F5; \
-                height:100%; text-align:center; display: flex; flex-direction: column; justify-content: space-evenly;">\
-                    <div>Total Staff This Pay</div>\
-                    <div>1,400</div>\
-                    <div style="font-size: 16px;color: green;">\
+                <div height="100%" style="border: 1px solid; border-radius: 5px; background-color: #F5F5F5; \
+                height:100%; text-align: center; display: flex; flex-direction: column; justify-content: space-evenly;">\
+                    <div style="font-size: 16px; font-weight: bold;">\
+                        Total Staff This Pay\
+                    </div>\
+                    <div style"font-size: 24px; font-weight: bold;">\
+                        1,400\
+                    </div>\
+                    <div style="font-size: 16px; color: green;">\
                         <i class="fa-solid fa-triangle" style="font-size: 30px;"></i>\
                         &nbsp;&nbsp;&nbsp;Up by 3\
                     </div>\
