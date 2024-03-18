af58053f5db7a53b5b0599ece386787d063b2795
alexsherin <alex@100email.co>
2024-03-18 18:30:37 +0000

    Alex dev commit

--- client_code/Pages/CompanyDashboardPage.py
+++ client_code/Pages/CompanyDashboardPage.py
@@ -11,7 +11,7 @@ class CompanyDashboardPage(DashboardPage):
         total_staff_widget_content = f'\
             <div style="padding: 10px; height:100%;">\
                 <div height="100%" style="border: 1px solid; border-radius: 5px; background-color:#F5F5F5; \
-                height:100%; text-align:center; display: flex; flex-direction: column; justify-content: center;">\
+                height:100%; text-align:center; display: flex; flex-direction: column; justify-content: space-evenly;">\
                     <div>Total Staff This Pay</div>\
                     <div>1,400</div>\
                     <div style="font-size: 16px;color: green;">\
