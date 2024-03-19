76dcee5f42c3f4215be9b0374f544086397bb398
alexsherin <alex@100email.co>
2024-03-19 20:28:49 +0000

    Alex dev commit

--- client_code/Pages/CompanyDashboardPage.py
+++ client_code/Pages/CompanyDashboardPage.py
@@ -27,7 +27,7 @@ class CompanyDashboardPage(DashboardPage):
             {'label': 'Vacation', 'value': 5},
         ]
         pay_distribution_chart = CircularChartWidget(title='Pay Distribution by Rate Type',
-                                                     chart_type='pie',
+                                                     chart_type='doughnut',
                                                      data=pay_distribution_data,
                                                      value_suffix='%',)
 
