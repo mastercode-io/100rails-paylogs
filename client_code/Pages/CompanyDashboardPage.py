b0067d978aedd17603da69bf6b9f3ecb24436d91
alexsherin <alex@100email.co>
2024-03-19 20:27:35 +0000

    Alex dev commit

--- client_code/Pages/CompanyDashboardPage.py
+++ client_code/Pages/CompanyDashboardPage.py
@@ -9,35 +9,9 @@ class CompanyDashboardPage(DashboardPage):
 
     def __init__(self, container_id, **kwargs):
 
-        # total_staff_widget_content = f'\
-        #     <div style="padding: 10px; height:100%;">\
-        #         <div height="100%" style="border: 1px solid; border-radius: 5px; border-color: lightgrey;\
-        #         background-color: #F5F5F5; height:100%; text-align: center; \
-        #         display: flex; flex-direction: column; justify-content: space-evenly;">\
-        #             <div style="font-size: 14px; font-weight: bold;">\
-        #                 Total Staff This Pay\
-        #             </div>\
-        #             <div style="font-size: 30px; font-weight: bold;">\
-        #                 1,400\
-        #             </div>\
-        #             <div style="font-size: 16px; color: green;">\
-        #                 <i class="fa-solid fa-caret-up" style="font-size: 70px;\
-        #                  position: relative; top: 20px; margin-top: -30px;"></i>\
-        #                 &nbsp;&nbsp;&nbsp;Up by 3\
-        #             </div>\
-        #         </div>\
-        #     </div>'
-        #
-        # total_pay_widget_content = f'\
-        #     <div style="padding: 10px;">\
-        #         <div style="border: 1px solid; background-color:grey;">\
-        #             <p>Total Paid This Pay</p>\
-        #             <p style="font-size:20px;font-weight:bold;">$34,544</p>\
-        #         </div>\
-        #     </div>'
         total_staff_widget = TickerWidget(title='Total Staff This Pay',
                                           value=1400,
-                                          change=3)
+                                          change=0)
 
         total_pay_widget = TickerWidget(title='Total Paid This Pay',
                                         value=34544,
@@ -52,7 +26,7 @@ class CompanyDashboardPage(DashboardPage):
             {'label': 'Sick', 'value': 5},
             {'label': 'Vacation', 'value': 5},
         ]
-        pay_distribution_chart = CircularChartWidget(title='Pay Distribution',
+        pay_distribution_chart = CircularChartWidget(title='Pay Distribution by Rate Type',
                                                      chart_type='pie',
                                                      data=pay_distribution_data,
                                                      value_suffix='%',)
