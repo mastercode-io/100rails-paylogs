adb68156cd21edc4704108d36b7de508c0b3037d
alexsherin <alex@100email.co>
2024-03-19 13:22:53 +0000

    Alex dev commit

--- client_code/Pages/CompanyDashboardPage.py
+++ client_code/Pages/CompanyDashboardPage.py
@@ -38,7 +38,7 @@ class CompanyDashboardPage(DashboardPage):
         total_staff_widget = TickerWidget('Total Staff This Pay', '1,400',
                                           ticket_change=3)
         total_pay_widget = TickerWidget('Total Paid This Pay', '$34,544',
-                                        ticket_change=-2568)
+                                        ticket_change=0)
 
         layout = {
             'showGridLines': True,
