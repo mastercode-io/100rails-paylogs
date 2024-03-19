370625730c49b4ae134d4913d130b2a46ee2babe
alexsherin <alex@100email.co>
2024-03-19 13:29:56 +0000

    Alex dev commit

--- client_code/Pages/widgets.py
+++ client_code/Pages/widgets.py
@@ -31,8 +31,8 @@ class TickerWidget:
         else:
             self.ticker_direction = f'\
                 <div style="font-size: 16px; color: grey;">\
-                    <i class="fa-solid fa-dash" style="font-size: 50px;\
-                     position: relative; top: 20px; margin-top: -30px;"></i>\
+                    <i class="fa-solid fa-dash" style="font-size: 30px;\
+                     position: relative; top: 0px; margin-top: -30px;"></i>\
                     &nbsp;&nbsp;&nbsp;No Change\
                 </div>'
 
