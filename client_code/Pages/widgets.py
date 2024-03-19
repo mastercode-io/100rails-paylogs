9dda4906a3e0b5777c07544c85371738661b2195
alexsherin <alex@100email.co>
2024-03-19 18:57:18 +0000

    Alex dev commit

--- client_code/Pages/widgets.py
+++ client_code/Pages/widgets.py
@@ -96,7 +96,7 @@ class CircularChartWidget:
         self.chart = ej.charts.AccumulationChart(chart_config)
 
         self.html = f'\
-            <div style="padding: 10px; text-align: center; height: auto">\
+            <div style="padding: 10px; text-align: center; display: flex; flex-direction: column; height: 100%;">\
                 <div>\
                     <div style="font-size: 14px; font-weight: bold;">{self.title}</div>\
                     <div id="{self._element_id}"></div>\
