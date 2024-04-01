6ec6f61a37fa3d305294df77bd25c7e5c45f5b65
alexsherin <alex@100email.co>
2024-04-01 19:11:30 +0100

    Alex auto-save

--- server_code/__init__.py
+++ server_code/__init__.py
@@ -1,6 +1,6 @@
-from anvil import AppEnvironment, AppInfo
-
-app_info = AppInfo()
-app_env = AppEnvironment()
-print('app environment:', app_env.name, app_env.tags)
-print('app info', app_info.branch, app_info.environment, app_info.id)
+# from anvil import AppEnvironment, AppInfo
+#
+# app_info = AppInfo()
+# app_env = AppEnvironment()
+# print('app environment:', app_env.name, app_env.tags)
+# print('app info', app_info.branch, app_info.environment, app_info.id)
--- server_code/api/__init__.py
+++ server_code/api/__init__.py
@@ -16,10 +16,10 @@ from Crypto.Cipher import AES
 from .resources import *
 from ..background_tasks import *
 
-app_env = anvil.AppEnvironment()
-app_info = anvil.AppInfo()
-print('app environment:', app_env.name, app_env.tags)
-print('app info', app_info.branch, app_info.environment, app_info.id)
+# app_env = anvil.AppEnvironment()
+# app_info = anvil.AppInfo()
+# print('app environment:', app_env.name, app_env.tags)
+# print('app info', app_info.branch, app_info.environment, app_info.id)
 app_api_origin = anvil.server.get_api_origin()
 if app_api_origin:
     app_env_list = app_tables.app_environments.search()
