16f8cb55007851ee21bb2223cb8ec6845752c5d1
alexsherin <alex@100email.co>
2024-04-01 19:04:40 +0100

    Alex dev commit

--- server_code/__init__.py
+++ server_code/__init__.py
@@ -1,6 +1,6 @@
-import anvil
+from anvil import AppEnvironment, AppInfo
 
-app_env = anvil.AppEnvironment()
-app_info = anvil.AppInfo()
+app_info = AppInfo()
+app_env = AppEnvironment()
 print('app environment:', app_env.name, app_env.tags)
 print('app info', app_info.branch, app_info.environment, app_info.id)
