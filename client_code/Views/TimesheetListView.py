d968224632752a0b7184d12a4b8fb6e2b063b4b2
alexsherin <alex@100email.co>
2024-04-02 19:40:19 +0100

    Alex auto-save

--- client_code/Views/TimesheetListView.py
+++ client_code/Views/TimesheetListView.py
@@ -87,12 +87,11 @@ class TimesheetListView(GridView):
 
     def query_cell_info(self, args):
         print('query_cell_info', args)
-        for k in args.column.keys():
-            print(k, args.column[k])
-        # if 'field' in args.colum.keys():
-        #     print(args.column, args.data)
-        #     if args.data['start_time'] is not None and args.data['end_time'] is not None:
-        #         pass
+        # for k in args.column.keys():
+        #     print(k, args.column[k])
+        if 'field' in args.column.keys() and args.column['field'] == 'end_time':
+            if args.data['start_time'] is not None and args.data['end_time'] is not None:
+                pass
                 # print(args.data['start_time'], args.data['end_time'])
                 # start_time = datetime.datetime.fromisoformat(args.data['start_time'])
                 # end_time = datetime.datetime.fromisoformat(args.data['end_time'])
