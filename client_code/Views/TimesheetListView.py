35e0831dcc99982f8fe047f847421e3f08496e20
alexsherin <alex@100email.co>
2024-04-02 19:19:47 +0100

    Alex auto-save

--- client_code/Views/TimesheetListView.py
+++ client_code/Views/TimesheetListView.py
@@ -90,9 +90,10 @@ class TimesheetListView(GridView):
         if 'field' in args.colum.keys() and args.column.field == 'end_time':
             if args.data['start_time'] is not None and args.data['end_time'] is not None:
                 # print(args.data['start_time'], args.data['end_time'])
-                start_time = datetime.datetime.fromisoformat(args.data['start_time'])
-                end_time = datetime.datetime.fromisoformat(args.data['end_time'])
-                plus_days = (end_time - start_time).days
+                # start_time = datetime.datetime.fromisoformat(args.data['start_time'])
+                # end_time = datetime.datetime.fromisoformat(args.data['end_time'])
+                # plus_days = (end_time - start_time).days
+                plus_days = 1
                 if plus_days > 0:
                     print('plus_days', plus_days)
                     for k in args.cell.keys():
