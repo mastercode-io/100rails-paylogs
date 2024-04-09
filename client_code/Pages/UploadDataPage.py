9fb57c3c7e2a58635de49f55fe9505d99d4aebc0
alexsherin <alex@100email.co>
2024-04-09 20:52:59 +0100

    Alex auto-save

--- client_code/Pages/UploadDataPage.py
+++ client_code/Pages/UploadDataPage.py
@@ -49,7 +49,7 @@ class UploadDataPage(PageBase):
     def __init__(self, **kwargs):
         print('UploadDataPage')
         title = 'Upload Data'
-        self.select_model = DropdownInput(name='select_model', label='Select Model', options=MODELS_LIST)
+        self.select_model = DropdownInput(name='select_model', label='Select Data Type', options=MODELS_LIST)
         self.upload_file = FileUploadInput(name='upload_file', label='Upload File', on_change=self.file_selected)
         self.import_button = ej.buttons.Button({
             'content': 'Import Records',
@@ -59,7 +59,6 @@ class UploadDataPage(PageBase):
         self.import_button_id = f'migrate-button-{uuid.uuid4()}'
         self.execution_log = InlineMessage(name='execution_log')
         self.record_count = InlineMessage(name='record_count')
-        print('DEBUG')
 
         self.content = f'<br><div id="{self.select_model.container_id}" style="width:300px;"></div>'
         self.content += f'<div id="{self.upload_file.container_id}" style="width:300px;"></div>'
