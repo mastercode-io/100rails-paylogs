23ebe077c286c192e8afd329d8fc956b68bcf2ed
alexsherin <alex@100email.co>
2024-04-09 17:30:52 +0100

    Alex auto-save

--- client_code/Views/TimesheetListView.py
+++ client_code/Views/TimesheetListView.py
@@ -17,7 +17,7 @@ class TimesheetListView(GridView):
         view_config = {
             'model': 'Timesheet',
             'columns': [
-                # {'name': 'payrun', 'label': 'Payrun'},
+                {'name': 'payrun', 'label': 'Payrun'},
                 {'name': 'employee.full_name', 'label': 'Employee Name'},
                 {'name': 'job.name', 'label': 'Job Name'},
                 {'name': 'job.job_type.short_code', 'label': 'Job Type'},
