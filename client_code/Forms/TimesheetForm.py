1b690a7065f7dbc489c36545eafa97f2a7d1a23c
alexsherin <alex@100email.co>
2024-04-14 20:58:53 +0100

    Alex auto-save

--- client_code/Forms/TimesheetForm.py
+++ client_code/Forms/TimesheetForm.py
@@ -42,7 +42,7 @@ class TimesheetForm(FormBase):
 
     def form_open(self, args, **kwargs):
         super().form_open(args, **kwargs)
-        print('TimesheetForm.form_open', self.data)
+        print('TimesheetForm.form_open', self.data['pay_lines'])
         if self.data['pay_lines']:
             self.pay_lines.content = self.data['pay_lines_view']
             self.pay_lines.show()
