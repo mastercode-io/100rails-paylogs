4ea6d5b6c0c89373d8604980fb94d62d4affddd3
alexsherin <alex@100email.co>
2024-04-14 18:45:16 +0100

    Alex auto-save

--- client_code/Forms/TimesheetForm.py
+++ client_code/Forms/TimesheetForm.py
@@ -9,17 +9,6 @@ class TimesheetForm(FormBase):
         print('TimesheetForm')
         kwargs['model'] = 'Timesheet'
 
-        # timesheet_type = Relationship("TimesheetType")
-        # employee = Relationship("Employee")
-        # payrun = Relationship("Payrun")
-        # job = Relationship("Job")
-        # date = Attribute(field_type=types.FieldTypes.DATE)
-        # start_time = Attribute(field_type=types.FieldTypes.DATETIME)
-        # end_time = Attribute(field_type=types.FieldTypes.DATETIME)
-        # status = Attribute(field_type=types.FieldTypes.ENUM_SINGLE)
-        # approved_by = Relationship("Employee")
-        # notes = Attribute(field_type=types.FieldTypes.MULTI_LINE)
-        # total_pay = Attribute(field_type=types.FieldTypes.CURRENCY)
         roles = [*EmployeeRole.search(name=q.any_of(q.ilike('%manager%'), q.ilike('%supervisor%')))]
         approved_by_filters = {
             'role': roles,
@@ -47,4 +36,4 @@ class TimesheetForm(FormBase):
 
         super().__init__(sections=sections, width=POPUP_WIDTH_COL2, **kwargs)
         if self.data['status'] == 'Draft':
-            self.allow_edit = False
+            self.action = 'view'
