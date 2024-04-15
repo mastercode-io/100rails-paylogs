5f4ef65045c5bd3f4952824fc66c6a057d2b3860
alexsherin <alex@100email.co>
2024-04-15 13:24:23 +0100

    Alex auto-save

--- client_code/app/HomePage/__init__.py
+++ client_code/app/HomePage/__init__.py
@@ -151,7 +151,7 @@ class HomePage(HomePageTemplate):
             self.appbar_menu.menu_items.extend(nav.PL_APPBAR_MENU_DEVELOPER)
         # if AppEnv.logged_user.permissions.developer:
         #     self.appbar_menu.menu_items.extend(nav.PL_APPBAR_MENU_DEVELOPER)
-        # self.appbar_menu.show()
+        self.appbar_menu.show()
 
         self.appbar_user_menu.items[0].text = AppEnv.logged_user.user_name + '<br>' + AppEnv.logged_user.email
         anvil.js.window.document.getElementById('pl-appbar-spacer').innerHTML = AppEnv.logged_user.tenant_name
