ecf0c5150ab57296663b9c7aca4f1451dcb2f6ce
alexsherin <alex@100email.co>
2024-04-16 15:04:30 +0100

    Alex auto-save

--- client_code/Forms/PayrunSettingsForm.py
+++ client_code/Forms/PayrunSettingsForm.py
@@ -41,7 +41,7 @@ class PayrunSettingsForm(FormBase):
         super().__init__(sections=sections, **kwargs)
         self.fullscreen = True
 
-        payrun_config = PayrunConfig.search().next()
+        payrun_config = next(PayrunConfig.search())
         print('payrun_config', payrun_config)
         if payrun_config:
             self.data = payrun_config
