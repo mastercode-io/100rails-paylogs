08a48774a4f7bceee989680e69a61769e8481f8b
alexsherin <alex@100email.co>
2024-04-19 15:27:35 +0100

    Alex auto-save

--- server_code/__init__.py
+++
@@ -1,6 +0,0 @@
-from anvil import app
-
-print('--- App Info ---')
-print(f'git branch: {app.branch}')
-print(f'environment: {app.environment.name} ({app.environment.tags})')
-print(f'id: {app.id}')
