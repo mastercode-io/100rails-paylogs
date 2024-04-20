8e3cfbbef23bc9bd0c43d2b9f8facfc1068f3bce
alex@100email.co <alex@100email.co>
2024-04-20 15:27:48 +0000

    Edited home-page.html, settings and tailwind.config.js

--- theme/assets/tailwind.config.js
+++ theme/assets/tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   plugins: [require("daisyui")],
-  daisyui: {
-    darkTheme: "light",
-  },
+    daisyui: {
+      themes: ["light"],
+      darkTheme: "light",
+    },
 }
