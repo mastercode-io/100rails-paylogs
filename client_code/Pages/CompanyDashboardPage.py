24896948dc2983ce829db54da933160ff53dbeb7
alexsherin <alex@100email.co>
2024-04-22 20:59:45 +0100

    Alex auto-save

--- client_code/Pages/CompanyDashboardPage.py
+++ client_code/Pages/CompanyDashboardPage.py
@@ -63,27 +63,27 @@ class CompanyDashboardPage(DashboardPage):
             'panels': [
                 {
                     'sizeX': 1, 'sizeY': 1, 'row': 0, 'col': 0,
-                    'id': 'total_staff_widget',
+                    'id': 'total_staff_stat',
                     'content': total_staff_stat.html,
                     'cssClass': PANEL_CSS_CLASS,
                 },
                 {
                     'sizeX': 1, 'sizeY': 1, 'row': 0, 'col': 1,
-                    'id': 'total_staff_widget',
+                    'id': 'total_hours_stat',
                     'content': total_hours_stat.html,
                     'cssClass': PANEL_CSS_CLASS,
                 },
-                {
-                    'sizeX': 1, 'sizeY': 1, 'row': 0, 'col': 2,
-                    'id': 'total_pay_widget',
-                    'content': total_pay_stat.html,
-                    'cssClass': PANEL_CSS_CLASS,
-                },
-                {
-                    'sizeX': 1, 'sizeY': 1, 'row': 0, 'col': 3,
-                    'id': 'total_pay_widget',
-                    'cssClass': PANEL_CSS_CLASS,
-                },
+                # {
+                #     'sizeX': 1, 'sizeY': 1, 'row': 0, 'col': 2,
+                #     'id': 'total_pay_stat',
+                #     'content': total_pay_stat.html,
+                #     'cssClass': PANEL_CSS_CLASS,
+                # },
+                # {
+                #     'sizeX': 1, 'sizeY': 1, 'row': 0, 'col': 3,
+                #     # 'id': 'total_pay_widget',
+                #     'cssClass': PANEL_CSS_CLASS,
+                # },
                 {
                     'sizeX': 2, 'sizeY': 2, 'row': 1, 'col': 0,
                     'id': 'pay_distribution_chart',
