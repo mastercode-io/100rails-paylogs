5c6ca4b004972bbd6d882d11093cbc0ca9efa969
alexsherin <alex@100email.co>
2024-04-22 16:22:53 +0100

    Alex auto-save

--- client_code/app/HomePage/__init__.py
+++ client_code/app/HomePage/__init__.py
@@ -24,7 +24,7 @@ AppEnv.theme = {
             'success': {'class': 'alert alert-success', 'icon': 'fa-solid fa-check-circle'},
             'info': {'class': 'alert alert-info', 'icon': 'fa-solid fa-info-circle'},
             'warning': {'class': 'alert alert-warning', 'icon': 'fa-solid fa-exclamation-circle'},
-            'error': {'class': 'alert alert-danger', 'icon': 'fa-solid fa-exclamation-triangle'},
+            'error': {'class': 'alert alert-error', 'icon': 'fa-solid fa-exclamation-triangle'},
         },
     }
 }
