5ea88040ced8ea46ba5cca6d96aae5524222f975
alexsherin <alex@100email.co>
2024-04-22 23:58:35 +0100

    Alex auto-save

--- client_code/Pages/widgets.py
+++ client_code/Pages/widgets.py
@@ -18,7 +18,7 @@ STAT_DOWN = 'error'
 STAT_UNCHANGED = 'neutral'
 ICON_UP = 'fa-solid fa-up-right'
 ICON_DOWN = 'fa solid fa-down-right'
-ICON_UNCHANGED = 'fa-duotone fa-arrows-up-down'
+ICON_UNCHANGED = 'fa-duotone fa-arrows-left-right'
 
 
 class TickerWidget:
