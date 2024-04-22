7c09fa24af6cac5b4477cc9d0e82a254a3f6b917
alexsherin <alex@100email.co>
2024-04-22 20:16:42 +0100

    Alex auto-save

--- client_code/Pages/widgets.py
+++ client_code/Pages/widgets.py
@@ -103,7 +103,7 @@ class StatWidget:
 
         self.html = f'\
             <div class="stats shadow">\
-                <div class="stat" style="font-size: 24px!important;">\
+                <div class="stat pl-stat-widget">\
                     <div class="stat-title">{self.title}</div>\
                     <div class="stat-value {STAT_ACCENTS[self.accent]}">{self.value_format.format(self.value)}</div>\
                     <div class="stat-desc">{self.description}</div>\
