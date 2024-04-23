ec67348623a261b3d344182d2c3ecdb8d0ca2dd2
alexsherin <alex@100email.co>
2024-04-23 20:58:40 +0100

    Alex auto-save

--- client_code/Forms/PayrunSettingsForm.py
+++ client_code/Forms/PayrunSettingsForm.py
@@ -30,7 +30,7 @@ class PayrunSettingsForm(FormBase):
         self.message = InlineMessage(css_class='pl-message-bar')
 
         self.payrun_flow_steps_schema = [
-            CheckboxInput(name='created', label='Created', value=True, enabled=True,
+            CheckboxInput(name='created', label='Created', value=True, enabled=False,
                           on_change=self.payrun_flow_changed),
             CheckboxInput(name='ts_entered', label='Timesheets Entered', value=False, enabled=True,
                           on_change=self.payrun_flow_changed),
