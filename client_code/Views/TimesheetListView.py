6efcea94878266e274382787e1ed73e5855b60fd
alexsherin <alex@100email.co>
2024-04-27 01:08:01 +0100

    Alex auto-save

--- client_code/Views/TimesheetListView.py
+++ client_code/Views/TimesheetListView.py
@@ -82,23 +82,23 @@ class TimesheetListView(GridView):
         anvil.js.window['captionTimesheetListView'] = self.grouping_caption
         # anvil.js.window['timesheetListGroupingTotalHours'] = self.grouping_total_hours
         self.grid.allowGrouping = True
-        # self.grid.groupSettings = {
-        #     'columns': ['employee__full_name'],
-        #     'showDropArea': False,
-        #     # 'captionTemplate': '<div>${key} - ${data}</div>',
-        #     # 'captionTemplate': '<div>${captionTimesheetListView(data)}</div>',
-        # }
-        # self.grid.aggregates = [{
-        #     'columns': [
-        #         {
-        #             'type': 'Custom',
-        #             'field': 'total_hours_view',
-        #             'columnName': 'total_hours_view',
-        #             'groupCaptionTemplate': '${Custom}',
-        #             'customAggregate': self.grouping_total_hours,
-        #         },
-        #     ],
-        # }]
+        self.grid.groupSettings = {
+            'columns': ['employee__full_name'],
+            'showDropArea': False,
+            # 'captionTemplate': '<div>${key} - ${data}</div>',
+            # 'captionTemplate': '<div>${captionTimesheetListView(data)}</div>',
+        }
+        self.grid.aggregates = [{
+            'columns': [
+                {
+                    'type': 'Custom',
+                    'field': 'total_hours_view',
+                    'columnName': 'total_hours_view',
+                    'groupCaptionTemplate': '${Custom}',
+                    'customAggregate': self.grouping_total_hours,
+                },
+            ],
+        }]
         self.grid.allowSorting = True
         self.grid.sortSettings = {
             'columns': [
