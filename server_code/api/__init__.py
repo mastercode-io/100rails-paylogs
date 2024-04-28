85d986072f89773ff4bcd087316cbda7a226e313
alexsherin <alex@100email.co>
2024-04-28 23:01:07 +0100

    Alex auto-save

--- server_code/api/__init__.py
+++ server_code/api/__init__.py
@@ -97,10 +97,10 @@ def generate_api_key(tenant_uid, api_service: AppIntegration):
     api_service_user.save()
 
     if api_service['direction'] == 'in' or api_service['direction'] == 'bidirectional':
-        api_credential = AppInApiCredential.search(api_service=api_service, api_user=api_service_user)
+        api_credential = AppInApiCredential.search(integration=api_service, api_user=api_service_user)
         if not api_credential:
             api_credential = AppInApiCredential(
-                api_service=api_service,
+                integration=api_service,
                 api_user=api_service_user,
             )
         api_secret = generate_password()
