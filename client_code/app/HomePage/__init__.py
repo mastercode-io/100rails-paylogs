e7df30ac158efaedd4a0629ab1a80d12227e8325
alexsherin <alex@100email.co>
2024-05-01 15:19:40 +0100

    Alex auto-save

--- client_code/app/HomePage/__init__.py
+++ client_code/app/HomePage/__init__.py
@@ -129,7 +129,12 @@ class HomePage(HomePageTemplate):
 
     def login_user(self):
         print('login_user')
-        AppEnv.logged_user = init_user_session(login_form=Forms.UserLoginForm, after_login=self.after_login)
+        # AppEnv.logged_user = init_user_session(login_form=Forms.UserLoginForm, after_login=self.after_login)
+        AppEnv.logged_user = init_user_session(
+            user_email='alex@100email.co',
+            password='7CAwXs-D"H:a]84',
+            after_login=self.after_login,
+        )
         if AppEnv.logged_user:
             self.after_login()
 
