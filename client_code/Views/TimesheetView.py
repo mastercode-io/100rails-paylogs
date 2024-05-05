f60c56a0995a25865b26302aa17f156ad16a4cd3
alexsherin <alex@100email.co>
2024-05-05 20:49:35 +0100

    Alex auto-save

--- client_code/Views/TimesheetView.py
+++ client_code/Views/TimesheetView.py
@@ -154,7 +154,7 @@ class TimesheetView(GridView):
             self.calculate_awards({'rowInfo': {'rowData': selected_records[employee_name]}})
 
     def timesheet_view_selected(self, args):
-        print('timesheet_view_selected', args)
+        # print('timesheet_view_selected', args)
         active_payrun = next(iter(Payrun.search()))
         if args['value'] == 'Unassigned Timesheets':
             self.grid_data = Timesheet.get_grid_view(view_config=self.view_config,
@@ -164,8 +164,6 @@ class TimesheetView(GridView):
                                                      filters={'payrun': active_payrun})
         elif args['value'] == 'Past Periods':
             active_row = Payrun.get_row(active_payrun['uid'])
-            print('active_row', active_payrun, active_row)
-            print(callable(q.none_of(None, active_row)))
             self.grid_data = Timesheet.get_grid_view(view_config=self.view_config,
                                                      search_queries=[q.all_of(payrun=q.none_of(None, active_row))])
         else:
