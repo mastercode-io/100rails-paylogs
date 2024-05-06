2c3f4e5af45c072d5cb5c6c14dbd0347b64d1fb7
alexsherin <alex@100email.co>
2024-05-06 21:52:19 +0100

    PL-1 Timesheet Views

--- client_code/Views/TimesheetView.py
+++ client_code/Views/TimesheetView.py
@@ -197,8 +197,8 @@ class TimesheetView(GridView):
             self.grid.columns[self.grid_column_indexes['payrun__payrun_week']].visible = True
             # self.grid.groupSettings = self.grid_group_settings['Past Periods']
             self.grid.ungroupColumn('employee__full_name')
-            self.grid.groupColumn('payrun__payrun_week')
-            self.grid.groupColumn('employee__full_name')
+            self.grid.groupColumn(['payrun__payrun_week', 'employee__full_name'])
+            # self.grid.groupColumn('employee__full_name')
             active_row = Payrun.get_row(self.active_payrun['uid'])
             self.grid_data = Timesheet.get_grid_view(view_config=self.view_config,
                                                      search_queries=[q.all_of(payrun=q.none_of(None, active_row))])
