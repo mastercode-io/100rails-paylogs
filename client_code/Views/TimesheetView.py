82980fe89ce905506319ae74dcc5f56b07f7af68
alexsherin <alex@100email.co>
2024-05-06 18:56:43 +0100

    PL-1 Timesheet Views

--- client_code/Views/TimesheetView.py
+++ client_code/Views/TimesheetView.py
@@ -118,13 +118,7 @@ class TimesheetView(GridView):
     def form_show(self, **args):
         print('TimesheetView.form_show')
         super().form_show(get_data=False, **args)
-        self.timesheet_view.value = 'Unassigned Timesheets'
-        # print(self.col_index)
-        # self.grid.columns[self.col_index['payrun.payrun_week']].visible = False
-        # for k in self.grid.columns[5].keys():
-        #     print(k, self.grid.columns[5][k])
-        # for col in self.grid.columns:
-        #     print(col, col.get('field'))
+        self.timesheet_view.value = 'Unassigned'
 
 
     def grouping_caption(self, args):
