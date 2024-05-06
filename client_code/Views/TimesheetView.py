ff971b0fa878b8c028232b5c429846cd7135704e
alexsherin <alex@100email.co>
2024-05-06 21:32:52 +0100

    PL-1 Timesheet Views

--- client_code/Views/TimesheetView.py
+++ client_code/Views/TimesheetView.py
@@ -76,7 +76,7 @@ class TimesheetView(GridView):
         ]
         self.grid_group_settings = {
             'Unassigned': {
-                'columns': ['date'],
+                'columns': ['job__name'],
                 'showDropArea': False,
                 # 'captionTemplate': '<div>${key} - ${data}</div>',
                 # 'captionTemplate': '<div>${captionTimesheetListView(data)}</div>',
@@ -187,8 +187,8 @@ class TimesheetView(GridView):
             self.grid.columns[self.grid_column_indexes['_selected']].visible = True
             self.grid.columns[self.grid_column_indexes['payrun__payrun_week']].visible = False
             # self.grid.groupSettings = self.grid_group_settings['Active Payrun']
-            self.grid.ungroupColumn('payrun__payrun_week')
-            self.grid.groupColumn('employee__full_name')
+            # self.grid.ungroupColumn('payrun__payrun_week')
+            # self.grid.groupColumn('employee__full_name')
             self.grid_data = Timesheet.get_grid_view(view_config=self.view_config,
                                                      filters={'payrun': self.active_payrun})
         elif args['value'] == 'Past Periods':
@@ -196,8 +196,8 @@ class TimesheetView(GridView):
             self.grid.columns[self.grid_column_indexes['_selected']].visible = False
             self.grid.columns[self.grid_column_indexes['payrun__payrun_week']].visible = True
             # self.grid.groupSettings = self.grid_group_settings['Past Periods']
-            self.grid.ungroupColumn('employee__full_name')
-            self.grid.groupColumn('date')
+            # self.grid.ungroupColumn('employee__full_name')
+            # self.grid.groupColumn('date')
             # self.grid.groupColumn('employee__full_name')
             active_row = Payrun.get_row(self.active_payrun['uid'])
             self.grid_data = Timesheet.get_grid_view(view_config=self.view_config,
