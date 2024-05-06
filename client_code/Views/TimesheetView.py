07cf01cbf60111f6b42ba9e61e0dce470c137a6f
alexsherin <alex@100email.co>
2024-05-06 23:15:25 +0100

    PL-1 Timesheet Views

--- client_code/Views/TimesheetView.py
+++ client_code/Views/TimesheetView.py
@@ -77,7 +77,7 @@ class TimesheetView(GridView):
         self.grid_group_settings = {
             'Unassigned': {
                 'columns': ['job__name', 'employee__full_name'],
-                'showDropArea': False,
+                # 'showDropArea': False,
                 # 'captionTemplate': '<div>${key} - ${data}</div>',
                 # 'captionTemplate': '<div>${captionTimesheetListView(data)}</div>',
             },
