9c2c3c5eae59b3ec39640efbe4744b10238be82f
alexsherin <alex@100email.co>
2024-05-07 16:46:30 +0100

    PL-1 Timesheet Views

--- client_code/Views/TimesheetView.py
+++ client_code/Views/TimesheetView.py
@@ -216,7 +216,7 @@ class TimesheetView(GridView):
         grouped_rows = self.grid.getContentTable().querySelectorAll('.e-recordplusexpand, .e-recordpluscollapse')
         print('grouped_rows', grouped_rows)
         for row in grouped_rows:
-            print(row)
+            self.grid.groupModule.expandCollapseRows(row)
 
 
     def assign_payrun_action(self, args):
