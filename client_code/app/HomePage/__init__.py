1d9481a93e58ba5d654ac9d2271a4f83cd2f9879
alexsherin <alex@100email.co>
2024-05-13 16:37:40 +0100

    PL-6 Multi-entity Accounts

--- client_code/app/HomePage/__init__.py
+++ client_code/app/HomePage/__init__.py
@@ -245,13 +245,13 @@ class HomePage(HomePageTemplate):
                 self.content_control = None
             # self.sidebar.show_menu(AppEnv.start_menu)
             self.login_user()
-            # self.appbar_user_menu.items[0].text = 'Sign In'
-            # self.appbar_user_menu.items[0].iconCss = 'fa-solid fa-sign-in'
-            # self.appbar_user_menu.items[0].id = 'pl-appbar-sign-in'
-        # elif args.item.id == 'pl-appbar-sign-in':
-        #     AppEnv.logged_user = init_user_session(login_form=Forms.UserLoginForm)
-        #     self.appbar_user_menu.items[0].text = AppEnv.logged_user['email']
-        #     self.appbar_user_menu.items[0].disabled = True
+            self.appbar_user_menu.items[0].text = 'Sign In'
+            self.appbar_user_menu.items[0].iconCss = 'fa-solid fa-sign-in'
+            self.appbar_user_menu.items[0].id = 'pl-appbar-sign-in'
+        elif args.item.id == 'pl-appbar-sign-in':
+            AppEnv.logged_user = init_user_session(login_form=Forms.UserLoginForm)
+            self.appbar_user_menu.items[0].text = AppEnv.logged_user['email']
+            self.appbar_user_menu.items[0].disabled = True
 
 
     def appbar_settings_menu_select(self, args):
