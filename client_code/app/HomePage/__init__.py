2bd5601a591f0503c910f7232f6b1d09c0093aa9
alexsherin <alex@100email.co>
2024-05-14 16:21:27 +0100

    PL-6 Multi-entity Accounts

--- client_code/app/HomePage/__init__.py
+++ client_code/app/HomePage/__init__.py
@@ -200,7 +200,7 @@ class HomePage(HomePageTemplate):
         #     "click", self.sidebar.toggle
         # )
         self.appbar_settings_button.appendTo(jQuery("#pl-appbar-settings-menu")[0])
-        self.appbar_settings_button.element.onclick = self.app.show_selected('settings_account')
+        self.appbar_settings_button.element.onclick = self.appbar_menu.show_selected('settings_account')
         self.appbar_assistant_toggle.appendTo(jQuery("#pl-appbar-assistant-toggle")[0])
         self.appbar_assistant_toggle.element.onclick = self.toggle
         # self.appbar_assistant_toggle.element.addEventListener(
