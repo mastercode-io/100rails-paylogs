ad7d271ced3cf13c9d46e506afe24be88b23c363
alexsherin <alex@100email.co>
2024-05-14 23:12:52 +0100

    PL-6 Multi-entity Accounts

--- client_code/Forms/AccountForm.py
+++ client_code/Forms/AccountForm.py
@@ -15,7 +15,7 @@ class AccountForm(FormBase):
 
         self.account = None
 
-        self.padding = InlineMessage(name='padding', label='_', content='&nbsp;')
+        self.padding = InlineMessage(name='padding', content='&nbsp;')
         self.account_name = TextInput(name='name', label='Account Name', required=True)
         self.business_name = TextInput(name='business_name', label='Business Name', required=True)
         self.address = MultiFieldInput(name='address', model='Account')
