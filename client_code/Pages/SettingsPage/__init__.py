86663080a59adf3072e2409126c2956f00880167
alexsherin <alex@100email.co>
2024-05-20 12:45:31 +0100

    PL-2 Payroll Settings and Payrun Setup

--- client_code/Pages/SettingsPage/__init__.py
+++ client_code/Pages/SettingsPage/__init__.py
@@ -17,7 +17,7 @@ class SettingsPage(PageBase):
             print(data_file['uid'], data_file['name'])
 
         if self.account is not None:
-            self.error_message = InlineMessage(content='No account found', accent='warning')
+            self.error_message = InlineMessage(content='No account found', accent='warning', css_class='pl-message-bar')
             self.content = f'<div id="{self.error_message.container_id}"></div>'
 
         elif len(self.account['data_files']) > 1:
