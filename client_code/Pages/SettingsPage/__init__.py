0ea8e273d06fe77143eb74934702b5c727d02ae0
alexsherin <alex@100email.co>
2024-05-20 16:58:01 +0100

    PL-2 Payroll Settings and Payrun Setup

--- client_code/Pages/SettingsPage/__init__.py
+++ client_code/Pages/SettingsPage/__init__.py
@@ -1,5 +1,6 @@
 from AnvilFusion.components.PageBase import PageBase
 from AnvilFusion.components.FormInputs import *
+from AnvilFusion.components.Layouts import Tabs
 from AnvilFusion.tools.utils import AppEnv
 from ...app.models import Account, Tenant, PayrollConfig
 
@@ -27,6 +28,14 @@ class SettingsPage(PageBase):
                                            options=options,
                                            value=AppEnv.logged_user.tenant_uid)
             self.content = f'<div id="{self.data_file.container_id}" style="width:300px;"></div>'
+            tabs_config = [
+                {'name': 'payroll', 'label': 'Payroll', 'content': 'Payroll Settings'},
+                {'name': 'timesheets', 'label': 'Timesheets', 'content': 'Timesheets Settings'},
+                {'name': 'leave', 'label': 'Leave', 'content': 'Leave Settings'},
+                {'name': 'expenses', 'label': 'Expenses', 'content': 'Expenses Settings'},
+            ]
+            self.tabs = Tabs(tabs_config=tabs_config)
+            self.content += f"<div id='{self.tabs.container_id}'></div>"
 
         super().__init__(page_title=title, content=self.content, overflow='auto', **kwargs)
 
@@ -37,3 +46,4 @@ class SettingsPage(PageBase):
             self.error_message.show()
         elif len(self.account['data_files']) > 1:
             self.data_file.show()
+            self.tabs.form_show()
