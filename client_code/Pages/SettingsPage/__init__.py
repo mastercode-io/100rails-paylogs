e8f469d1eaeb53c86b846c2a8de4673a3e904090
alexsherin <alex@100email.co>
2024-05-24 15:38:49 +0100

    PL-2 Payroll Settings and Payrun Setup

--- client_code/Pages/SettingsPage/__init__.py
+++ client_code/Pages/SettingsPage/__init__.py
@@ -90,7 +90,7 @@ class SettingsPage(PageBase):
             self.data_file.value = AppEnv.logged_user.tenant_uid
             self.data_fil2.value = AppEnv.logged_user.tenant_uid
             self.data_fil2.control.enableEditMode = True
-            self.data_fil2.control.enableEditMode = False
+            # self.data_fil2.control.enableEditMode = False
             self.data_fil2.action_success({'value': self.current_data_file['name']})
 
 
