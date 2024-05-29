a406b97a2c112632bf0687f6cd6ede9cc30b15c8
alexsherin <alex@100email.co>
2024-05-29 20:43:50 +0100

    PL-2 Payroll Settings and Payrun Setup

--- client_code/Pages/SettingsPage/__init__.py
+++ client_code/Pages/SettingsPage/__init__.py
@@ -22,34 +22,6 @@ class SettingsPage(PageBase):
             self.content = f'<div id="{self.error_message.container_id}"></div>'
 
         else:
-            options = [df.to_json_dict() for df in self.account['data_files']]
-            options.append({'uid': 'all', 'name': 'All Data Files'})
-            self.data_file = DropdownInput(name='data_file',
-                                           label='Select Data File',
-                                           options=options,
-                                           on_change=self.data_file_selected)
-            self.data_fil2 = DropdownInput(name='data_file',
-                                           label='Select Data File',
-                                           options=options,
-                                           inplace_mode='Inline',
-                                           # value=current_data_file,
-                                           on_change=self.data_file_selected2)
-            self.text_input = TextInput(name='text_input',
-                                        label='Text Input',
-                                        value='Text Input Value',
-                                        inplace_mode='Inline')
-            self.number_input = NumberInput(name='number_input',
-                                            label='Number Input',
-                                            value='123',
-                                            inplace_mode='Inline')
-            self.multiline_input = MultiLineInput(name='multiline_input',
-                                                  label='Multiline',
-                                                  value='Some text',
-                                                  inplace_mode='Inline')
-            self.date_input = DateInput(name='date_input',
-                                        label='Date Input',
-                                        value=datetime.date.today(),
-                                        inplace_mode='Inline')
 
             tabs_config = [
                 {'name': 'payroll', 'label': 'Payroll', 'content': 'Payroll Settings'},
@@ -59,14 +31,6 @@ class SettingsPage(PageBase):
             ]
             self.tabs = Tabs(tabs_config=tabs_config)
 
-            self.content = f'<div id="{self.data_file.container_id}" style="width: 300px;"></div>'
-            self.content += f'<div id="{self.data_fil2.container_id}"></div>'
-            self.content += f'<div id="{self.text_input.container_id}"></div>'
-            self.content += f'<div id="{self.number_input.container_id}"></div>'
-            self.content += f'<div id="{self.multiline_input.container_id}"></div>'
-            self.content += f'<div id="{self.date_input.container_id}"></div>'
-            self.content += f"<div id='{self.tabs.container_id}'></div>"
-
         super().__init__(page_title=title, content=self.content, overflow='auto', **kwargs)
 
 
@@ -74,27 +38,3 @@ class SettingsPage(PageBase):
         super().form_show(**args)
         if self.account is None:
             self.error_message.show()
-        elif len(self.account['data_files']) > 1:
-            self.data_file.show()
-            self.data_fil2.show()
-            self.text_input.show()
-            self.number_input.show()
-            self.multiline_input.show()
-            self.date_input.show()
-            self.tabs.form_show()
-            self.data_file.value = AppEnv.logged_user.tenant_uid
-            self.data_fil2.value = AppEnv.logged_user.tenant_uid
-            print('inline', args)
-            print(self.data_fil2.value)
-            # self.data_fil2.action_success({'value': self.current_data_file['name']})
-
-
-    def data_file_selected(self, args):
-        print('dropdown', args)
-        print(self.data_file.value)
-
-    def data_file_selected2(self, args):
-        print('inline', args)
-        print(self.data_fil2.value)
-        # for k in self.data_fil2.control.keys():
-        #     print(k, self.data_fil2.control[k])
