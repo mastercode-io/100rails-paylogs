6d7bb0b1245f70579dc378f0e14f816f068aa6a2
alexsherin <alex@100email.co>
2024-05-29 18:52:59 +0100

    PL-2 Payroll Settings and Payrun Setup

--- client_code/app/HomePage/__init__.py
+++ client_code/app/HomePage/__init__.py
@@ -195,7 +195,7 @@ class HomePage(HomePageTemplate):
         if AppEnv.logged_user.app_mode == 'Super Admin Mode' or AppEnv.logged_user.app_mode == 'Developer Mode':
             tenant_list = models.Tenant.search(tenant_uid=None, search_query=anvil.tables.order_by('account_uid'))
             AppEnv.logged_user.data_files = [df.to_json_dict() for df in tenant_list]
-        if self.account is not None:
+        elif self.account is not None:
             AppEnv.logged_user.data_files = [df.to_json_dict() for df in self.account['data_files']]
         else:
             tenant_list = models.Tenant.search()
