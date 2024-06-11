141c19cd5f48dfe30ec64bb37dec167932361307
alexsherin <alex@100email.co>
2024-06-11 16:43:34 +0100

    PL-2 Payroll Settings and Payrun Setup

--- client_code/app/HomePage/__init__.py
+++ client_code/app/HomePage/__init__.py
@@ -187,7 +187,11 @@ class HomePage(HomePageTemplate):
         #     self.appbar_menu.menu_items.extend(nav.PL_APPBAR_MENU_DEVELOPER)
         self.appbar_menu.show()
 
-        self.appbar_user_menu.items[0].text = AppEnv.logged_user.user_name + '\n' + AppEnv.logged_user.email
+        self.appbar_user_menu.items[0].text = AppEnv.logged_user.user_name + ' ' + AppEnv.logged_user.email
+        print('appbar_user_menu.item props')
+        for k in self.appbar_user_menu.items[0].keys():
+            print(k, self.appbar_user_menu.items[0][k])
+
         anvil.js.window.document.getElementById('pl-appbar-spacer').innerHTML = AppEnv.logged_user.app_mode
 
         tenant = models.Tenant.get(AppEnv.logged_user.tenant_uid)
