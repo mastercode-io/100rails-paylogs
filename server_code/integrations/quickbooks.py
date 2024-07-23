7030644b76fd520bbe15229a142d5d7ebfbb8296
alexsherin <alex@100email.co>
2024-07-23 22:02:42 +0100

    PL-2 Payroll Settings and Payrun Setup

--- server_code/integrations/quickbooks.py
+++ server_code/integrations/quickbooks.py
@@ -4,7 +4,7 @@ import anvil.users
 import anvil.secrets
 from intuitlib.client import AuthClient
 from intuitlib.enums import Scopes
-from ..app.models import Tenant, AppIntegration, AppOutApiCredential
+from ..app.models import Account, Tenant, AppIntegration, AppOutApiCredential
 import json
 
 
@@ -38,17 +38,20 @@ def qb_auth(**params):
     print(f"qb_access_token: {qb_access_token}\nqb_refresh_token: {qb_refresh_token}")
     print(f"tenant_uid: {tenant_uid}\nrealm_id: {realm_id}")
 
-    qb_integration = AppIntegration.get_by('service_name', 'QuickBooks')
-    AppOutApiCredential(
-        tenant_uid=tenant_uid,
-        integration=qb_integration,
-        api_credentials={
-            'access_token': qb_access_token,
-            'refresh_token': qb_refresh_token,
-            'realm_id': realm_id,
-        },
-        status='Active',
-    ).save()
+    tenant = Tenant.get_row(tenant_uid)
+    account = next(iter(Account.search(pay_entities=[tenant])), None)
+    # qb_integration = AppIntegration.get_by('service_name', 'QuickBooks')
+    # AppOutApiCredential(
+    #     tenant_uid=tenant_uid,
+    #     integration=qb_integration,
+    #     api_credentials={
+    #         'access_token': qb_access_token,
+    #         'refresh_token': qb_refresh_token,
+    #         'realm_id': realm_id,
+    #     },
+    #     status='Active',
+    # ).save()
+
 
     # return anvil.server.HttpResponse(200, "OK")
-    return anvil.server.FormResponse('app.HomePage', start_page='settings_form', start_props={})
+    return anvil.server.FormResponse('app.HomePage', start_page='settings_form', start_props={'data': account})
