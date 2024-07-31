e46f16a636aa6e96badb4dc3797f07cdd121cc2a
alexsherin <alex@100email.co>
2024-07-31 21:49:31 +0100

    PL-2 Payroll Settings and Payrun Setup

--- client_code/app/HomePage/navigation.py
+++ client_code/app/HomePage/navigation.py
@@ -113,7 +113,7 @@ PL_NAV_ITEMS = {
     'admin_users': {'model': 'User', 'type': 'view', 'action': 'open', 'props': {}},
     'admin_settings_scope_types': {'model': 'ScopeType', 'type': 'view', 'action': 'open', 'props': {}},
     'admin_integrations': {'model': 'AppIntegration', 'type': 'view', 'action': 'open', 'props': {}},
-    # 'admin_user_roles': {'model': 'UserRole', 'type': 'view', 'action': 'open', 'props': {}},
+    'admin_user_roles': {'model': 'UserRole', 'type': 'view', 'action': 'open', 'props': {}},
     # 'admin_permissions': {'model': 'Permission', 'type': 'view', 'action': 'open', 'props': {}},
     # 'admin_settings': {'model': 'Setting', 'type': 'view', 'action': 'open', 'props': {}},
 
