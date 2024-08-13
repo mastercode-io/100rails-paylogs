80a54a1c03f54defe5e4f8d7147b94837c673d32
alexsherin <alex@100email.co>
2024-08-13 18:53:39 +0100

    PL-2 Payroll Settings and Payrun Setup

--- client_code/app/HomePage/__init__.py
+++ client_code/app/HomePage/__init__.py
@@ -216,7 +216,8 @@ class HomePage(HomePageTemplate):
         print('user role type', AppEnv.logged_user.user_role_type)
         user_app_menu = nav.get_user_menu_items(
             nav.PL_MENU_ITEMS_2,
-            nav.DEFAULT_USER_PERMISSIONS['user_roles'][AppEnv.logged_user.user_role_type]['permissions']['app_menu'])
+            # nav.DEFAULT_USER_PERMISSIONS['user_roles'][AppEnv.logged_user.user_role_type]['permissions']['app_menu'])
+            nav.DEFAULT_USER_PERMISSIONS['user_roles']['payroll_manager']['permissions']['app_menu'])
         print('user_app_menu', user_app_menu)
         self.appbar_menu.menu_items = user_app_menu
         self.appbar_menu.show()
